c903049ef03ec393f421d4eaa67b6a446f613c17
gamedevmarz <mariobadr@gmail.com>
2018-06-21 14:10:05 -0400

    Move initialization/shutdown code to main

--- src/engine/windowManager.cxx
+++ src/engine/windowManager.cxx
@@ -11,35 +11,11 @@ WindowManager::~WindowManager()
 {
   SDL_DestroyRenderer(_renderer);
   SDL_DestroyWindow(_window);
-  TTF_Quit();
-  SDL_Quit();
-  Mix_Quit();
 }
 
 bool WindowManager::init()
 {
-  // Initialize SDL
-  if (SDL_Init(SDL_INIT_VIDEO) != 0)
-  {
-    LOG(LOG_ERROR) << "Failed to Init SDL\nSDL Error:" << SDL_GetError();
-    return false;
-  }
-
-  // Initialize SDL_TTF
-  if (TTF_Init() == -1)
-  {
-    LOG(LOG_ERROR) << "Failed to Init SDL_TTF\nSDL Error:" << TTF_GetError();
-    return false;
-  }
-
-  if (Mix_Init(MIX_INIT_MP3) == -1)
-  {
-    LOG(LOG_ERROR) << "Failed to Init SDL_Mixer\nSDL Error:" << Mix_GetError();
-    return false;
-  }
-
   _window = SDL_CreateWindow(_title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _width, _height, 0);
-
   if (_window == nullptr)
   {
     LOG(LOG_ERROR) << "Failed to Init SDL\nSDL Error:" << SDL_GetError();
@@ -47,7 +23,6 @@ bool WindowManager::init()
   }
 
   _renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED);
-
   if (_renderer == nullptr)
   {
     LOG(LOG_ERROR) << "Failed to create Renderer!\nSDL Error:" << SDL_GetError();
--- src/main.cxx
+++ src/main.cxx
@@ -1,9 +1,57 @@
 #include "game.hxx"
+#include "engine/basics/log.hxx"
 
-int main(int, char**)
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+
+bool initialize()
 {
-  // run the game
-  run();
+  if (SDL_Init(SDL_INIT_VIDEO) != 0)
+  {
+    LOG(LOG_ERROR) << "Failed to Init SDL\nSDL Error:" << SDL_GetError();
+    return false;
+  }
 
-  return 0;
+  if (TTF_Init() == -1)
+  {
+    LOG(LOG_ERROR) << "Failed to Init SDL_TTF\nSDL Error:" << TTF_GetError();
+    return false;
+  }
+
+  if (Mix_Init(MIX_INIT_MP3) == -1)
+  {
+    LOG(LOG_ERROR) << "Failed to Init SDL_Mixer\nSDL Error:" << Mix_GetError();
+    return false;
+  }
+
+  return true;
+}
+
+void shutdown()
+{
+  TTF_Quit();
+  Mix_Quit();
+  SDL_Quit();
+}
+
+int main(int, char **)
+{
+  // initialize all systems and globals
+  auto const initialized = initialize();
+
+  if (initialized)
+  {
+    // run the game
+    run();
+  }
+  else
+  {
+    return EXIT_FAILURE;
+  }
+
+  // shutdown all systems and clean-up stuff
+  shutdown();
+
+  return EXIT_SUCCESS;
 }
