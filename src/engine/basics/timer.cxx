1558226a3e30438ed84b8875675593b44a30455d
JimmySnails <talantyyr@gmail.com>
2018-10-21 17:40:42 +0200

    Timer enhancements - Timer can now execute callback functions on timeout

--- src/engine/basics/timer.cxx
+++ src/engine/basics/timer.cxx
@@ -1,24 +1,44 @@
 #include "timer.hxx"
 
+Timer::~Timer()
+{
+  _threadRunning = false;
+  if (_timerThread.joinable())
+  {
+    _timerThread.join();
+  }
+}
+
 void Timer::start()
 {
   _startTime = SDL_GetPerformanceCounter();
+  _lastTimeOutTime = SDL_GetPerformanceCounter();
   _endTime = _startTime;
   _elapsedTime = 0;
   _isActive = true;
+  _timeOut = false;
+
+  startThread();
 }
 
 void Timer::stop()
 {
-  _elapsedTime = (double)((SDL_GetPerformanceCounter() - _startTime) * 1000 / SDL_GetPerformanceFrequency());
+  _elapsedTime = (int)((SDL_GetPerformanceCounter() - _startTime) * 1000 / SDL_GetPerformanceFrequency());
   _isActive = false;
+  _timeOut = false;
+  _threadRunning = false;
+  if (_timerThread.joinable())
+  {
+    _timerThread.join();
+  }
 }
 
 void Timer::pause()
 {
   if (_isActive)
   {
-    _elapsedTime = (double)((SDL_GetPerformanceCounter() - _startTime) * 1000 / SDL_GetPerformanceFrequency());
+    _elapsedTime = (int)((SDL_GetPerformanceCounter() - _startTime) * 1000 / SDL_GetPerformanceFrequency());
+    _timeSinceLastTimeOut = (int)((SDL_GetPerformanceCounter() - _lastTimeOutTime) * 1000 / SDL_GetPerformanceFrequency());
     _endTime = _startTime;
     _startTime = SDL_GetPerformanceCounter();
     _isActive = false;
@@ -30,15 +50,75 @@ void Timer::resume()
   if (!_isActive)
   {
     _startTime = SDL_GetPerformanceCounter() - (_startTime - _endTime);
+    _lastTimeOutTime = SDL_GetPerformanceCounter() - _lastTimeOutTime;
     _isActive = true;
   }
 }
 
-double Timer::getElapsedTime()
+void Timer::setTimer(int timeInMs)
+{
+  _timeUntilTimeOut = timeInMs;
+  _timeOut = false;
+}
+
+int Timer::getElapsedTime()
 {
   if (_isActive)
   {
-    _elapsedTime = (double)((SDL_GetPerformanceCounter() - _startTime) * 1000 / SDL_GetPerformanceFrequency());
+    _elapsedTime = (int)((SDL_GetPerformanceCounter() - _startTime) * 1000 / SDL_GetPerformanceFrequency());
   }
   return _elapsedTime;
-};
+}
+
+int Timer::getElapsedTimeSinceLastTimeOut()
+{
+  if (_isActive)
+  {
+    _timeSinceLastTimeOut = (int)((SDL_GetPerformanceCounter() - _lastTimeOutTime) * 1000 / SDL_GetPerformanceFrequency());
+  }
+  return _timeSinceLastTimeOut;
+}
+
+void Timer::timeOut()
+{
+  _timeSinceLastTimeOut = 0;
+  _timeOut = true;
+  _timeOutSignal.emit();
+  _lastTimeOutTime = SDL_GetPerformanceCounter();
+}
+
+void Timer::startThread()
+{
+  // if thread is running, abort and join it before starting a new one
+  if (_timerThread.joinable())
+  {
+    _threadRunning = false;
+    _timerThread.join();
+  }
+  else if (_threadRunning == true)
+  {
+    return;
+  }
+
+  _threadRunning = true;
+  if (_timeUntilTimeOut != 0)
+  {
+    _timerThread = std::thread([=]() {
+      while (_threadRunning)
+      {
+        if (getElapsedTimeSinceLastTimeOut() >= _timeUntilTimeOut)
+        {
+          timeOut();
+          if (!_loopTimer)
+          {
+            _threadRunning = false;
+          }
+        }
+
+        // helps with cpu usage...
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+      }
+      return;
+    });
+  }
+}
--- src/engine/basics/timer.hxx
+++ src/engine/basics/timer.hxx
@@ -3,24 +3,36 @@
 
 #include <ctime>
 #include <chrono>
+#include <thread>
+#include <future>
 
 #include "SDL.h"
 #include "../basics/log.hxx"
+#include "../basics/signal.hxx"
 
 class Timer
 {
 public:
   /** \brief Timer object
-  * creates a Timer object, that can measure past time in milliseconds
+  * creates a Timer object, that can measure past time in milliseconds or set a 
+  * countdown timer. It's also possible to invoke a callback function when the timer is triggered
   */
   Timer() = default;
-  ~Timer() = default;
+  ~Timer();
 
   /** \brief get elapsed time
   * Returns the time in milliseconds, that has passed since the timer was started.
   * @returns the time in milliseconds, that has passed since the timer was started
   */
-  double getElapsedTime();
+  int getElapsedTime();
+
+  /** \brief get elapsed time since last timeOut
+  * Returns the time in milliseconds, that has passed since the last TimeOut has happend.
+  * @returns the time in milliseconds, that has passed since the last timeOut
+  * @note Only applicable, if a timer has been set
+  * @see Timer#setTimer()
+  */
+  int getElapsedTimeSinceLastTimeOut();
 
   /** \brief Start / Reset the timer
   * Starts or restarts the timer.
@@ -42,16 +54,62 @@ public:
   */
   void resume();
 
-  /** \bref Checks if the timer is active.
+  /** \brief Checks if the timer is active.
   * Returns true if the timer is running (pending); otherwise returns false.
   */
   bool isActive() { return _isActive; };
 
+  /** \brief Checks if the time is done.
+  * Returns true, if the timer has reached 0. 
+  * @note Only applicable, if a timer has been set
+  * @see Timer#setTimer()
+  */
+  bool isTimedOut() { return _timeOut; };
+
+  /** \brief Sets a timer
+   * Sets a timer. When a timer is set, a signal is emitted and the registered callback function 
+   * is called. Also a bool variable will be set to true.
+   * @see Timer#egisterCallbackFunction
+   * @see Timer#isTimedOut()
+  */
+  void setTimer(int timeInMs);
+
+  /** \brief Register callback function for this timer
+  * Register one or more callback functions that should be exececuted, when the Timer is triggered.
+  * @note Only applicable, if a timer has been set
+  * @see setTimer()
+  * @see Signal#Signal
+  * @see Signal#slot
+  */
+  void registerCallbackFunction(std::function<void()> const &cb) { _timeOutSignal.connect(cb); };
+
+  /** Loop Timer
+  * Specify if the timerout callback is called once or everytime the counter hits zero.
+  */
+  void loopTimer(bool loop) { _loopTimer = loop; };
+
 private:
+  void startThread();
+
+  Signal::Signal<void()> _timeOutSignal;
+
+  std::thread _timerThread;
+
+  bool _loopTimer = false;
+  bool _timeOut = false;
+  bool _isActive = false;
+  bool _threadRunning = false;
+
+  int _timeUntilTimeOut = 0;
+  int _timeSinceLastTimeOut = 0;
+  int _elapsedTime = 0;
+
   Uint64 _startTime = 0;
   Uint64 _endTime = 0;
-  double _elapsedTime = 0;
-  bool _isActive = false;
+
+  Uint64 _lastTimeOutTime = 0;
+
+  void timeOut();
 };
 
 #endif
