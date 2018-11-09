262415c6d6a6faf4b194904ec4bf0c5571234b9d
JimmySnails <talantyyr@gmail.com>
2018-11-09 19:17:12 +0100

    add 2 new structs to represent tile data with default values.

--- src/engine/tile.hxx
+++ src/engine/tile.hxx
@@ -4,6 +4,31 @@
 #include "SDL2/SDL.h"
 #include <bitset>
 
+struct TileSetData
+{
+  std::string fileName;
+  int count = 1;
+  int clippingWidth = 0;
+  int clippingHeight = 0;
+};
+
+struct TileInformation
+{
+  std::string id;
+  std::string type;
+  TileSetData tiles;
+  TileSetData cornerTiles;
+  TileSetData slopeTiles;
+  std::string title;
+  std::string description;
+  int price = 0;      // building cost
+  int upkeepCost = 0; // monthly cost
+  int power = 0;      // power production / consumption if negative
+  int water = 0;      // water production / consumption if negative
+  std::string author;
+  bool drawGround = false; // draw ground below sprite (grass, flowers, ...)
+};
+
 class Tile
 {
 public:
