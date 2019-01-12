643895d8ccc1a593bab11ee859c551bf808c484c
JimmySnails <talantyyr@gmail.com>
2019-01-12 18:47:19 +0100

    Move tiledata structs into seperate header file so that the json editor application can use the same structs. Also added defines for min / max values for certain attributes

---
+++ src/engine/basics/tileData.hxx
@@ -0,0 +1,49 @@
+#ifndef TILEDATA_HXX_
+#define TILEDATA_HXX_
+
+#include <string>
+
+
+/// min and max values for tileData
+#define TD_PRICE_MIN 1
+#define TD_PRICE_MAX 100000
+#define TD_UPKEEP_MIN 1
+#define TD_UPKEEP_MAX 10000
+#define TD_POWER_MIN -100
+#define TD_POWER_MAX 1000
+#define TD_WATER_MIN -100
+#define TD_WATER_MAX 1000
+#define TD_TITLE_MAX_CHARS 100
+#define TD_DESCRIPTION_MAX_CHARS 100
+#define TD_AUTHOR_MAX_CHARS 100
+#define TD_ID_MAX_CHARS 100
+#define TD_TYPE_MAX_CHARS 40
+
+
+struct TileSetData
+{
+  std::string fileName;
+  int count = 1;
+  int clippingWidth = 0;
+  int clippingHeight = 0;
+};
+
+struct TileData
+{
+  std::string id;
+  std::string type;
+  TileSetData tiles;
+  TileSetData cornerTiles;
+  TileSetData slopeTiles;
+  std::string title;
+  std::string description;
+  int price = 0;      // building cost
+  int upkeepCost = 0; // monthly cost
+  int power = 0;      // power production / consumption if negative
+  int water = 0;      // water production / consumption if negative
+  std::string author;
+  bool drawGround = false; // draw ground below sprite (grass, flowers, ...)
+};
+
+
+#endif
--- src/engine/tileManager.hxx
+++ src/engine/tileManager.hxx
@@ -1,35 +1,11 @@
-#ifndef TILE_HXX_
-#define TILE_HXX_
+#ifndef TILEMANAGER_HXX_
+#define TILEMANAGER_HXX_
 
+#include "basics/tileData.hxx"
 #include "SDL2/SDL.h"
 
 #include <unordered_map>
 #include <string>
-
-struct TileSetData
-{
-  std::string fileName;
-  int count = 1;
-  int clippingWidth = 0;
-  int clippingHeight = 0;
-};
-
-struct TileData
-{
-  std::string id;
-  std::string type;
-  TileSetData tiles;
-  TileSetData cornerTiles;
-  TileSetData slopeTiles;
-  std::string title;
-  std::string description;
-  int price = 0;      // building cost
-  int upkeepCost = 0; // monthly cost
-  int power = 0;      // power production / consumption if negative
-  int water = 0;      // water production / consumption if negative
-  std::string author;
-  bool drawGround = false; // draw ground below sprite (grass, flowers, ...)
-};
 
 enum TileMap : size_t
 {
