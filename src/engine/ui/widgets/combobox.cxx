be4efa632c8b57cda264b556f1627755d98cb1c8
JimmySnails <talantyyr@gmail.com>
2019-03-02 12:51:33 +0100

    TextFields getSelectedID() function does not need parameters.

--- src/engine/ui/widgets/combobox.cxx
+++ src/engine/ui/widgets/combobox.cxx
@@ -111,7 +111,7 @@ void ComboBox::onMouseButtonUp(const SDL_Event &event)
 
   if (_isMenuOpened)
   {
-    _activeID = _textField->getSeletectedID(x, y);
+    _activeID = _textField->getSeletectedID();
     _activeText = _textField->getTextFromID(_activeID);
     _isMenuOpened = false;
     _textField->setVisibility(false);
