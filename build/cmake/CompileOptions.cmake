e29f00b4472baa3388de8731c0a1bf051355c55f
JimmySnails <talantyyr@gmail.com>
2019-03-02 15:24:14 +0100

    Disable warning 4996 in Compile options instead of using #pragma

--- build/cmake/CompileOptions.cmake
+++ build/cmake/CompileOptions.cmake
@@ -24,6 +24,8 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
         /W4           # -> warning level 3
         ${WARN_AS_ERROR_FLAGS}
 
+
+        /wd4996       # -> disable warning: 'localtime': This function or variable may be unsafe. Consider using localtime_s instead. 
         # /wd4251     # -> disable warning: 'identifier': class 'type' needs to have dll-interface to be used by clients of class 'type2'
         # /wd4592     # -> disable warning: 'identifier': symbol will be dynamically initialized (implementation limitation)
         # /wd4201     # -> disable warning: nonstandard extension used: nameless struct/union (caused by GLM)
