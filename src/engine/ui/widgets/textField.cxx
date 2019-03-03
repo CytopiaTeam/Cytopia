a2802baf31dc82aabc2c6def34011c8a19af91a7
JimmySnails <talantyyr@gmail.com>
2019-03-03 18:52:09 +0100

    add a private count variable and a const function to retrieve it. This removes the redundant vector.size() calls

--- src/engine/ui/widgets/textField.cxx
+++ src/engine/ui/widgets/textField.cxx
@@ -32,7 +32,7 @@ void TextField::addText(const std::string &text)
 
   _textVector.push_back(new Text(text));
   textRect.h = _textVector.back()->getUiElementRect().h; // get height of text after instantiating
-  textRect.y = _uiElementRect.y + ((static_cast<int>(_textVector.size()) - 1) * textRect.h);
+  textRect.y = (_uiElementRect.y + _count - 1) * textRect.h;
 
   // center text
   if (centerText)
@@ -50,11 +50,12 @@ void TextField::addText(const std::string &text)
   _hoverRect.h = _textElementHeight;
 
   _textVector.back()->setPosition(textRect.x, textRect.y);
+  _count = static_cast<int>(_textVector.size());
 }
 
 std::string TextField::getTextFromID(int id)
 {
-  if (static_cast<int>(id) < static_cast<int>(_textVector.size()))
+  if (id < _count)
   {
     return _textVector[id]->getUiElementData().text;
   }
@@ -67,23 +68,24 @@ void TextField::onMouseButtonUp(const SDL_Event &event)
   {
     _selectedID = ((_textElementHeight + event.button.y - _uiElementRect.y) / _textElementHeight) - 1;
     // because of the -4 pixel offset that's been added in the constructor, the id would exceed the size of the vector, if the bottom of the dropdown is clicked
-    if (_selectedID > static_cast<int>(_textVector.size()))
+
+    if (_selectedID >= _count)
     {
-      _selectedID = static_cast<int>(_textVector.size());
+      _selectedID = _count - 1;
     }
   }
 }
 
 void TextField::onMouseMove(const SDL_Event &event)
 {
-  // TODO: Calculation deviates by 4 pixel in height !
+  // TODO: Calculation deviates by 4 pixel in height because of the frame
   if (_hoveredID != ((_textElementHeight + event.button.y - _uiElementRect.y) / _textElementHeight) - 1)
   {
     _hoveredID = ((_textElementHeight + event.button.y - _uiElementRect.y) / _textElementHeight) - 1;
     // because of the -4 pixel offset that's been added in the constructor, the id would exceed the size of the vector, if the bottom of the dropdown is clicked
-    if (_hoveredID > static_cast<int>(_textVector.size()) - 1)
+    if (_hoveredID >= _count)
     {
-      _hoveredID = static_cast<int>(_textVector.size()) - 1;
+      _hoveredID = _count - 1;
     }
     _hoverRect.y = ((_hoveredID)*_textElementHeight) + _uiElementRect.y;
   }
