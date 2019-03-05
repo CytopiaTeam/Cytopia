e6971d1edb5d1e98f6f502eb8a2cc85f06e24fda
galegosimpatico <galegosimpatico@outlook.com>
2019-03-05 19:11:40 +0100

    Fake loading lines.

--- src/game.cxx
+++ src/game.cxx
@@ -35,6 +35,45 @@ void run()
   Uint32 fpsLastTime = SDL_GetTicks();
   Uint32 fpsFrames = 0;
 
+  // Fake loading screen.
+  unsigned short i = 0;
+  srand(time(NULL));
+  const unsigned short MAX_FAKE_LOADING_SCREEN_MILLIS = rand() % 500 + 500;
+  const unsigned char TTF_INIT_FAILED = 0xFE;
+  const unsigned char TTF_OPEN_FONT_FAILED = 0xFF;
+  std::vector<std::string> loading_lines = std::vector<std::string>();
+  loading_lines.push_back("loading  traffic");
+  loading_lines.push_back("calling  aliens");
+  loading_lines.push_back("heating  volcanoes");
+  const unsigned char chosen_line_index = rand() % loading_lines.size();
+  const char * chosen_loading_line =
+      loading_lines.at(chosen_line_index).c_str();
+  char * pct_as_string = (char *) malloc(10);
+  while (engine.isGameRunning() && i < MAX_FAKE_LOADING_SCREEN_MILLIS)
+  {
+    SDL_RenderClear(WindowManager::instance().getRenderer());
+
+    evManager.checkEvents(event, engine);
+
+    Text loading_line(SDL_Rect{320, 240, 120, 40});
+    loading_line.setText(chosen_loading_line);
+    loading_line.draw();
+
+    Text pct_Text(SDL_Rect{360, 260, 80, 60});
+    const unsigned char pct = i * 100 / MAX_FAKE_LOADING_SCREEN_MILLIS / 10 * 10;
+    sprintf(pct_as_string, "%u", pct);
+    strcat(pct_as_string, "%");
+    pct_Text.setText(pct_as_string);
+    pct_Text.draw();
+
+    // Render the Frame
+    SDL_RenderPresent(WindowManager::instance().getRenderer());
+
+    SDL_Delay(1);
+    i++;
+  }
+  free(pct_as_string);
+
   // Gameloop
   while (engine.isGameRunning())
   {
