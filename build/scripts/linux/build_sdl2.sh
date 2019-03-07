b2cabde969e5f675139ac19200e6a2b3beac0929
JimmySnails <talantyyr@gmail.com>
2019-03-07 21:37:38 +0100

    move sdl build commands to seperate script and adjust travis yml further

---
+++ build/scripts/linux/build_sdl2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Build SDL2 manually to have the latest version
+curl -L https://www.libsdl.org/release/SDL2-2.0.9.tar.gz | tar xz
+pushd SDL2-* && ./configure && make && sudo make install && popd
+curl -L https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.tar.gz | tar xz
+pushd SDL2_ttf-* && ./configure && make && sudo make install && popd
+curl -L https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.4.tar.gz | tar xz
+pushd SDL2_image-* && ./configure && make && sudo make install && popd
+curl -L https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.tar.gz | tar xz
+pushd SDL2_mixer-* && ./configure && make && sudo make install && popd
+popd
