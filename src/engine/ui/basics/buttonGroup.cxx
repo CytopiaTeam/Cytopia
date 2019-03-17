59bf6db6ed588d2abf1cbccfe6b58b1fccb1f63d
JimmySnails <talantyyr@gmail.com>
2019-03-17 10:37:14 +0100

    Add event handling to ButtonGroup class

--- src/engine/ui/basics/buttonGroup.cxx
+++ src/engine/ui/basics/buttonGroup.cxx
@@ -1,3 +1,20 @@
 #include "buttonGroup.hxx"
+#include "../../basics/log.hxx"
 
 void ButtonGroup::addToGroup(UiElement *widget) { m_buttonGroup.push_back(widget); }
+
+void ButtonGroup::onMouseButtonDown(const SDL_Event &event)
+{
+  for (auto &it : m_buttonGroup)
+  {
+    it->onMouseButtonDown(event);
+  }
+}
+
+void ButtonGroup::onMouseButtonUp(const SDL_Event &event)
+{
+  for (auto &it : m_buttonGroup)
+  {
+    it->onMouseButtonUp(event);
+  }
+}
