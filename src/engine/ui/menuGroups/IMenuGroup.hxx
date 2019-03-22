7d6ac8bf2a463c7e83ce36da3a8fe73340d31b47
JimmySnails <talantyyr@gmail.com>
2019-03-22 14:52:52 +0100

    Add more containers to IMenuGroup Interface and check if the object is not nullptr before adding.

--- src/engine/ui/menuGroups/IMenuGroup.hxx
+++ src/engine/ui/menuGroups/IMenuGroup.hxx
@@ -1,6 +1,8 @@
 #include "../basics/uiElement.hxx"
-#include <vector>
+#include "../basics/buttonGroup.hxx"
 
+#include <map>
+#include <vector>
 /**
  * @brief Interface Class for Menu Groups
  * Menu Groups can hold UiElements and format them in a special way. 
@@ -12,14 +14,20 @@ public:
    * @brief Pure virtual function for drawing the Menu Group
    * Draw all elements that are within this group. Must be implemented in the derived class.
    */
-  virtual void draw () const = 0;
+  virtual void draw() const = 0;
 
   /**
    * @brief Add an UiElement* to the container.
    * 
    * @param widget Pointer to an UiElement
    */
-  void addToGroup(UiElement *widget) { m_groupElements.push_back(widget); };
+  void addToGroup(Button *widget)
+  {
+    if (widget)
+    {
+      m_groupElements.push_back(widget);
+    }
+  };
 
   /**
    * @brief Visbility of group.
@@ -28,10 +36,14 @@ public:
   bool visible = true;
 
 protected:
-/**
+  /**
  * @brief container for storing UiElements
  * 
  */
-  std::vector<UiElement *> m_groupElements;
+  std::vector<Button *> m_groupElements;
 
+  std::unordered_map<std::string, ButtonGroup*> m_buildSubMenuGroups;
+  // This buttongroup holds the main elements
+  // TODO: Shared or unique? 
+  ButtonGroup *m_buildMenuGroup = new ButtonGroup;
 };
