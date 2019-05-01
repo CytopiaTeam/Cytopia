921fd48d3d268bfcfd2f1fa3dcb3e617a1f66c5a
JimmySnails <talantyyr@gmail.com>
2019-05-01 11:42:56 +0200

    add include guards to Constants.hxx

--- src/engine/common/Constants.hxx
+++ src/engine/common/Constants.hxx
@@ -1,2 +1,7 @@
+#ifndef CONSTANTS_HXX_
+#define CONSTANTS_HXX_
+
 constexpr char SETTINGS_FILE_NAME[] = "resources/settings.json";
 constexpr char TERRAINGEN_DATA_FILE_NAME[] = "resources/data/TerrainGen.json";
+
+#endif
