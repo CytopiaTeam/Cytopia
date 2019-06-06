e998f292a657543502e204a553e65913e80a0fac
Vincent-Olivier Roch <vroch@edu.uwaterloo.com>
2019-06-06 15:55:12 -0400

    Adding documentation and scenarios

--- src/engine/AudioMixer.hxx
+++ src/engine/AudioMixer.hxx
@@ -7,17 +7,44 @@
 #include "audio/Music.hxx"
 #include "audio/SoundEffect.hxx"
 
+/**
+ * @class AudioMixer
+ * @todo this should be a singleton
+ */
 class AudioMixer
 {
 public:
   AudioMixer();
   ~AudioMixer() = default;
 
-  void setMusicVolume(int volume);
-  void setSoundEffectVolume(int volume);
-  void playMusic();
+  /**
+   * @brief sets the music volume
+   * @pre   volume must be within [0, 128]
+   * @post  Settings::MusicVolume is changed
+   */
+  void setMusicVolume(uint8_t volume) noexcept;
 
-  void mute();
+  /**
+   * @brief sets the sound effects volume
+   * @pre   volume must be within [0, 128]
+   * @post  Settings::SoundEffectsVolume is changed
+   * @todo  this doesn't do anything. must find a way to
+   *        actually deal with SDL2_Mixer
+   */
+  void setSoundEffectVolume(uint8_t volume) noexcept;
+
+  /**
+   * @brief starts theme music
+   * @todo  stored audio elements should be moved 
+   *        to the resources class
+   */
+  void playMusic(void) noexcept;
+
+  /**
+   * @brief stops all sounds
+   * @todo  stop all music stored in the element class 
+   */
+  void mute(void) noexcept;
 
 private:
   std::vector<std::shared_ptr<Music>> m_musicObjects;
--- test/engine/AudioMixer.cxx
+++ test/engine/AudioMixer.cxx
@@ -6,7 +6,7 @@
 template <class T>
 using Limits = std::numeric_limits<T>;
 
-TEST_CASE("Create an destroy AudioMixer", "[engine]") {
+TEST_CASE("Create an destroy AudioMixer", "[engine][audio]") {
   AudioMixer* mixer = nullptr;
   REQUIRE_NOTHROW(
       mixer = new AudioMixer
@@ -16,7 +16,7 @@ TEST_CASE("Create an destroy AudioMixer", "[engine]") {
   );
 }
 
-TEST_CASE("Set the music volume", "[engine]") {
+TEST_CASE("Set the music volume", "[engine][audio]") {
   AudioMixer mixer;
   REQUIRE_NOTHROW(
     mixer.setMusicVolume(10)
@@ -35,7 +35,7 @@ TEST_CASE("Set the music volume", "[engine]") {
   );
 }
 
-TEST_CASE("Set sound effects volume", "[engine]") {
+TEST_CASE("Set sound effects volume", "[engine][audio]") {
   AudioMixer mixer;
   REQUIRE_NOTHROW(
     mixer.setSoundEffectVolume(10)
@@ -54,10 +54,89 @@ TEST_CASE("Set sound effects volume", "[engine]") {
   );
 }
 
-TEST_CASE("Play music", "[engine]") {
+TEST_CASE("Play music", "[engine][audio]") {
   AudioMixer mixer;
   REQUIRE_NOTHROW(
     mixer.playMusic()
   );
+}
+
+SCENARIO("I can change the music volume", "[engine][audio]") {
+  GIVEN("The music volume is currently 0") {
+    AudioMixer mixer;
+    mixer.setMusicVolume(0);
+    REQUIRE(Settings::instance().settings.musicVolume == 0);
+    WHEN("I set the music volume to 128") {
+      mixer.setMusicVolume(128);
+      THEN("My music volume setting becomes 128") {
+        REQUIRE(Settings::instance().settings.musicVolume == 128);
+      }
+    }
+  }
+  GIVEN("The music volume is currently 128") {
+    Audiomixer mixer;
+    mixer.setMusicVolume(128);
+    REQUIRE(Settings::instance().settings.musicVolume == 128);
+    WHEN("I set the music volume to 0") {
+      mixer.setMusicVolume(0);
+      THEN("My music volume setting becomes 0") {
+        REQUIRE(Settings::instance().settings.musicVolume == 0);
+      }
+    }
+  }
+}
+
+SCENARIO("I can change the sound effects volume", "[engine][audio]") {
+  GIVEN("The sound effects volume is currently 0") {
+    AudioMixer mixer;
+    mixer.setSoundEffectsVolume(0);
+    REQUIRE(Settings::instance().settings.soundEffectsVolume == 0);
+    WHEN("I set the sound effects volume to 128") {
+      mixer.setSoundEffectsVolume(128);
+      THEN("My sound effects volume setting becomes 128") {
+        REQUIRE(Settings::instance().settings.soundEffectsVolume == 128);
+      }
+    }
+  }
+  GIVEN("The sound effects volume is currently 128") {
+    Audiomixer mixer;
+    mixer.setSoundEffectsVolume(128);
+    REQUIRE(Settings::instance().settings.soundEffectsVolume == 128);
+    WHEN("I set the sound effects volume to 0") {
+      mixer.setSoundEffectsVolume(0);
+      THEN("My sound effects volume setting becomes 0") {
+        REQUIRE(Settings::instance().settings.soundEffectsVolume == 0);
+      }
+    }
+  }
+}
+
+SCENARIO("I can play music", "[engine][audio]") {
+  GIVEN("I am not hearing anything") {
+    AudioMixer mixer;
+    mixer.mute();
+    REQUIRE(Mix_Playing(-1) == 0);
+    WHEN("I play music") {
+      mixer.playMusic();
+      THEN("I hear music") {
+        REQUIRE(Mix_Playing(-1) > 0);
+      }
+    }
+  }
+}
+
+SCENARIO("I can mute sounds", "[engine][audio]") {
+  GIVEN("I am playing something") {
+    Audiomixer mixer;
+    mixer.plauMusic();
+    REQUIRE(Mix_Playing(-1) > 0);
+    WHEN("I mute all sounds") {
+      AudioMixer mixer;
+      mixer.mute();
+      THEN("I am not hearing anything") {
+        REQUIRE(Mix_Playing(-1) == 0);
+      }
+    }
+  }
 }
 
