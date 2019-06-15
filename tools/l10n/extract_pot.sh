16a24be6cd0cdc08e788e1396ff199eaeb884efc
Edgar <Edgar@AnotherFoxGuy.com>
2019-06-15 16:44:54 +0200

    :wrench: improved translation scripts

--- tools/l10n/extract_pot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-
-# extract strings preceded by a specific macro to a .pot file as a template for translations
-cd ../../
-find src/TileDataUi -iname "*.cxx" | xargs xgettext -odata/languages/TileDataUi.pot -ktr -k"_LC:1c,2" -c -s -j
--- tools/l10n/setup_transifex.sh
+++ tools/l10n/setup_transifex.sh
@@ -2,14 +2,8 @@
 set -eu
 
 # Set's up local repo for Transifex
-
-if [ -d tx ]; then
-	rm -r tx
-fi
-
-mkdir tx
+mkdir tx;
 cd tx
-
 tx init --host=www.transifex.com --no-interactive
 tx set --auto-remote https://www.transifex.com/projects/p/cytopia
-tx pull -a
+
