bbc87815916bb1ce67272ced18da7f07ea5b018e
JimmySnails <talantyyr@gmail.com>
2019-06-16 21:40:54 +0200

    Added more tests

--- tests/ui/widgets/Text.cxx
+++ tests/ui/widgets/Text.cxx
@@ -1,22 +1,23 @@
 #include <catch2/catch.hpp>
 #include "../../src/engine/ui/widgets/Text.hxx"
+#include "../../src/engine/basics/Settings.hxx"
 
 using string = std::string;
 
-TEST_CASE("Create a Text widget", "[widgets]")
+TEST_CASE("I can create a Text widget", "[ui][widgets][text]")
 {
   Text *text = new Text;
   CHECK(text != nullptr);
 }
 
-TEST_CASE("I can set text on a Text widget", "[widgets]")
+TEST_CASE("I can set text on a Text widget", "[ui][widgets][text]")
 {
   Text text;
   text.setText("Test");
   CHECK(text.getUiElementData().text == "Test");
 }
 
-TEST_CASE("I can change the font size of a Text widget", "[widgets]")
+TEST_CASE("I can change the font size of a Text widget", "[ui][widgets][text]")
 {
   Text text;
   text.setFontSize(30);
@@ -24,4 +25,21 @@ TEST_CASE("I can change the font size of a Text widget", "[widgets]")
   text.setFontSize(100);
   CHECK(text.getUiElementData().text == "Test");
   CHECK(text.getFontSize() == 100);
+}
+
+TEST_CASE("I can draw a Text widget", "[ui][widgets][text]")
+{
+  Text *text = new Text;
+  text->draw();
+  text->setText("This is a test");
+  text->draw();
+  CHECK(text != nullptr);
+}
+
+TEST_CASE("I can't load the font", "[ui][widgets][!mayfail]")
+{
+  Text *text = new Text;
+  Settings::instance().fontFileName = "I DO NOT EXIST";
+  text->draw();
+  CHECK(text != nullptr);
 }
