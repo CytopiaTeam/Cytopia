d227f8d75e0814970cf8269a732045eba22931ec
JimmySnails <talantyyr@gmail.com>
2019-06-23 19:21:24 +0200

    add basic Engine test and a test that checks for renderer creation

---
+++ tests/engine/Engine.cxx
@@ -0,0 +1,19 @@
+#include <catch2/catch.hpp>
+#include "../../src/engine/Engine.hxx"
+
+using string = std::string;
+
+TEST_CASE("Get Engine instance", "[engine][engine]")
+{
+  void *singleton1 = static_cast<void *>(&Engine::instance());
+  void *singleton2 = static_cast<void *>(&Engine::instance());
+  CHECK(singleton1 == singleton2);
+}
+
+//TEST_CASE("Start Engine", "[engine][engine]")
+//{
+//  Engine &engine = Engine::instance();
+//  engine.newGame();
+//  engine.quitGame();
+//  CHECK(engine.isGameRunning() == false);
+//}
---
+++ tests/engine/WindowManager.cxx
@@ -0,0 +1,38 @@
+#include <catch2/catch.hpp>
+#include "../../src/engine/WindowManager.hxx"
+
+#include "SDL.h"
+#include <string>
+
+using string = std::string;
+
+TEST_CASE("Get WindowManager instance", "[engine][WindowManager]")
+{
+  void *singleton1 = static_cast<void *>(&WindowManager::instance());
+  void *singleton2 = static_cast<void *>(&WindowManager::instance());
+  CHECK(singleton1 == singleton2);
+}
+
+TEST_CASE("Create SDL_Renderer and SDL_Window", "[engine][WindowManager]")
+{
+  WindowManager &windowManager = WindowManager::instance();
+  CHECK(windowManager.getRenderer() != nullptr);
+  CHECK(windowManager.getWindow() != nullptr);
+}
+
+TEST_CASE("Toggle Fullscreen", "[engine][WindowManager]")
+{
+  WindowManager &windowManager = WindowManager::instance();
+  windowManager.toggleFullScreen();
+  ///@todo: check for fullscreenmode here!
+  CHECK(windowManager.getRenderer() != nullptr);
+  CHECK(windowManager.getWindow() != nullptr);
+}
+
+TEST_CASE("Set Window Title", "[engine][WindowManager]")
+{
+  WindowManager &windowManager = WindowManager::instance();
+  const std::string windowTitle = "_TEST_"; 
+  windowManager.setWindowTitle(windowTitle);
+  CHECK(windowTitle == SDL_GetWindowTitle(windowManager.getWindow()));
+}
