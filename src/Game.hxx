405dfd608910d0d2fb41f44899d32682f89429ff
Elizabeth Sherrock <lizzyd710@gmail.com>
2019-06-28 10:10:07 -0400

    Added documentation for the methods of Game.hxx

--- src/Game.hxx
+++ src/Game.hxx
@@ -12,10 +12,23 @@ class Game
 public:
   Game() = default;
   virtual ~Game() = default;
-
+  
+  /** @brief starts setting up the game
+    * starts game initialization.
+    */
   virtual bool initialize();
+  /** @brief begins the game
+    * starts running the game
+    * @param SkipMenu if the main menu should be skipped or not
+    */
   virtual void run(bool SkipMenu = false);
+  /** @brief ends the game
+    * shuts down the game
+    */
   virtual void shutdown();
+  /** @brief initializes and displays the main menu
+    * initializes and displays the main menu
+    */
   virtual void mainMenu();
 };
 
