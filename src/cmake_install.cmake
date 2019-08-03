71352cea91408e80cc31ecc72f34f6157a57c0d5
Pablo Camacho <bringerofawesomefood@gmail.com>
2019-08-03 09:07:28 -0700

    Removed Cytopia_Resources directory from commit

--- src/cmake_install.cmake
+++ src/cmake_install.cmake
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "")
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
