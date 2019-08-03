6ed1246afd049b26e6a080524b5ac7453b3d4d0c
Pablo Camacho <bringerofawesomefood@gmail.com>
2019-08-03 09:10:51 -0700

    Ignore CMakeFiles.

--- src/CMakeFiles/copy_resources.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_resources"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_resources.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
