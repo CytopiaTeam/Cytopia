ee3f79641ab41afb7c746db8cff62a6da052c3bc
Pablo Camacho <bringerofawesomefood@gmail.com>
2019-12-06 09:27:14 -0700

    Added 3D audio option to game sounds.

--- src/Game.cxx
+++ src/Game.cxx
@@ -99,7 +99,10 @@ void Game::mainMenu()
   newGameButton.setUIElementID("newgame");
   newGameButton.registerCallbackFunction([this]() {
     m_AudioMixer.stopAll();
-	m_AudioMixer.play(SoundtrackID{"MajorSelection"});
+    
+    if(!Settings::instance().audio3DStatus){m_AudioMixer.play(SoundtrackID{"MajorSelection"});}
+    else{m_AudioMixer.play(SoundtrackID{"MajorSelection"},Coordinate3D{0,0,-4});}
+	
 	Engine::instance().newGame(); 
   });
 
@@ -107,7 +110,8 @@ void Game::mainMenu()
   loadGameButton.setText("Load Game");
   loadGameButton.registerCallbackFunction([this]() {
 	m_AudioMixer.stopAll();
-	m_AudioMixer.play(SoundtrackID{"MajorSelection"}); 
+	if(!Settings::instance().audio3DStatus){m_AudioMixer.play(SoundtrackID{"MajorSelection"});}
+    else{m_AudioMixer.play(SoundtrackID{"MajorSelection"},Coordinate3D{0,0,-4});} 
 	Engine::instance().loadGame("resources/save.cts"); 
   });
 
@@ -115,7 +119,8 @@ void Game::mainMenu()
   quitGameButton.setText("Quit Game");
   quitGameButton.registerCallbackFunction([this]() {
     m_AudioMixer.stopAll();
-	m_AudioMixer.play(SoundtrackID{"NegativeSelect"}); 
+	if(!Settings::instance().audio3DStatus){m_AudioMixer.play(SoundtrackID{"NegativeSelect"});}
+    else{m_AudioMixer.play(SoundtrackID{"NegativeSelect"},Coordinate3D{0,0,-4});} 
 	Engine::instance().quitGame(); 
   });
 
@@ -239,8 +244,21 @@ void Game::run(bool SkipMenu)
 #endif
 
 #ifdef USE_SDL2_MIXER
-  m_GameClock.createRepeatedTask(8min, [this]() { m_AudioMixer.play(AudioTrigger::MainTheme); });
-  m_GameClock.createRepeatedTask(3min, [this]() { m_AudioMixer.play(AudioTrigger::NatureSounds); });
+  
+  //if not playing in 3D audio
+  if (!Settings::instance().audio3DStatus)
+  {
+	m_GameClock.createRepeatedTask(8min, [this]() { m_AudioMixer.play(AudioTrigger::MainTheme); });
+	m_GameClock.createRepeatedTask(3min, [this]() { m_AudioMixer.play(AudioTrigger::NatureSounds); });
+  }
+  //else if playing in 3D audio
+  else
+  {
+	m_GameClock.createRepeatedTask(8min, [this]() { m_AudioMixer.play(AudioTrigger::MainTheme,Coordinate3D{0, 0, -6}); });
+	m_GameClock.createRepeatedTask(3min, [this]() { m_AudioMixer.play(AudioTrigger::NatureSounds,Coordinate3D{0, 0, -4}); });
+  }
+  
+  
 #endif
 
   // FPS Counter variables
