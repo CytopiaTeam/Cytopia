e253837b08889be3750184d62469fc4835cbacaa
JimmySnails <talantyyr@gmail.com>
2020-02-20 19:24:03 +0100

    Fix windows packaging and adjust project links

--- cmake/CpackConfiguration.cmake
+++ cmake/CpackConfiguration.cmake
@@ -10,7 +10,7 @@ set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/license.txt")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "A pixel-art city building game")
 set(CPACK_PACKAGE_VENDOR "Cytopia")
 
-set(CPACK_PACKAGE_ICON "${PROJECT_SOURCE_DIR}/build/msvc/cytopia.ico")
+set(CPACK_PACKAGE_ICON "${PROJECT_SOURCE_DIR}/src/Windows/cytopia.ico")
 
 set(CPACK_STRIP_FILES ON)
 set(CPACK_PACKAGE_INSTALL_DIRECTORY ${PROJECT_NAME})
@@ -48,14 +48,15 @@ elseif ( WIN32 )
   set(CPACK_WIX_UPGRADE_GUID "F56FA877-A127-415C-9D04-43023C5459B0")
   set(CPACK_WIX_PRODUCT_ICON "${CPACK_PACKAGE_ICON}")
   set(CPACK_WIX_CMAKE_PACKAGE_REGISTRY ${PROJECT_NAME})
-  set(CPACK_WIX_UI_BANNER "${PROJECT_SOURCE_DIR}/build/setup/banner.png")
-  set(CPACK_WIX_UI_DIALOG "${PROJECT_SOURCE_DIR}/build/setup/logoSide.png")
+  set(CPACK_WIX_UI_BANNER "${PROJECT_SOURCE_DIR}/data/setup/banner.png")
+  set(CPACK_WIX_UI_DIALOG "${PROJECT_SOURCE_DIR}/data/setup/logoSide.png")
   set(CPACK_WIX_PROGRAM_MENU_FOLDER "${PROJECT_NAME}")
 
   #For Windows Start Menu entries
   set(CPACK_PACKAGE_EXECUTABLES
     "${PROJECT_NAME}" "${PROJECT_NAME}"
-    "TileDataEditor" "TileDataEditor"
+    # Disabled, the TileData Editor has been moved to a seperate repository and is probably broken at this time.
+    # "TileDataEditor" "TileDataEditor"
   )
 
   #For Windows Desktop shortcuts
@@ -65,9 +66,9 @@ elseif ( WIN32 )
 
   # Windows Add or Remove Program properties
   set(CPACK_WIX_PROPERTY_ARPCOMMENTS "${CPACK_PACKAGE_DESCRIPTION_SUMMARY}")
-  set(CPACK_WIX_PROPERTY_ARPHELPLINK "https://github.com/JimmySnails/Cytopia/wiki")
-  set(CPACK_WIX_PROPERTY_ARPURLINFOABOUT "https://github.com/JimmySnails/Cytopia")
-  set(CPACK_WIX_PROPERTY_URLUPDATEINFO "https://github.com/JimmySnails/Cytopia/releases/")
+  set(CPACK_WIX_PROPERTY_ARPHELPLINK "https://github.com/CytopiaTeam/Cytopia/wiki")
+  set(CPACK_WIX_PROPERTY_ARPURLINFOABOUT "https://github.com/CytopiaTeam/Cytopia")
+  set(CPACK_WIX_PROPERTY_URLUPDATEINFO "https://github.com/CytopiaTeam/Cytopia/releases/")
 endif()
 
 include(CPack)
