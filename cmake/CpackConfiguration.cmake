d54895665dbd1ead26239a6ffd19d628a739047c
JimmySnails <talantyyr@gmail.com>
2020-02-20 13:09:52 +0100

    Re-add Cpack packaging for Linux (RPM and DEB platforms)

--- cmake/CpackConfiguration.cmake
+++ cmake/CpackConfiguration.cmake
@@ -15,7 +15,33 @@ set(CPACK_PACKAGE_ICON "${PROJECT_SOURCE_DIR}/build/msvc/cytopia.ico")
 set(CPACK_STRIP_FILES ON)
 set(CPACK_PACKAGE_INSTALL_DIRECTORY ${PROJECT_NAME})
 
-if ( WIN32 )
+if ( UNIX AND NOT DEFINED $ENV{TRAVIS})
+  if ( (linux_distribution MATCHES Debian) OR (linux_distribution MATCHES Ubuntu) )
+    configure_file(${PROJECT_SOURCE_DIR}/data/setup/cytopia.desktop setup/cytopia.desktop)
+    configure_file(${PROJECT_SOURCE_DIR}/data/setup/cytopia-tiledata-editor.desktop setup/cytopia-tiledata-editor.desktop)
+
+    install(FILES ${PROJECT_BINARY_DIR}/setup/cytopia.desktop DESTINATION /usr/share/applications)
+    install(FILES ${PROJECT_BINARY_DIR}/setup/cytopia-tiledata-editor.desktop DESTINATION /usr/share/applications)
+
+    set(CPACK_PACKAGING_INSTALL_PREFIX "/usr/games/${PROJECT_NAME}")
+    set(CPACK_GENERATOR "DEB")
+	
+  	set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+  	set(CPACK_DEBIAN_PACKAGE_SECTION games)
+    set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Jimmy Snails")
+    set(CPACK_DEBIAN_PACKAGE_HOMEPAGE ${CPACK_PACKAGE_HOMEPAGE_URL})
+	
+  elseif ( (linux_distribution MATCHES CentOS) OR (linux_distribution MATCHES SUSE))
+    set(CPACK_GENERATOR "RPM")
+    # todo: 
+	  # - add more debian / rpm specific generator settings
+  elseif(APPLE)
+    set(CPACK_GENERATOR "DragNDrop")
+    set(CPACK_DMG_VOLUME_NAME "${PROJECT_NAME}")
+  else ()
+    set(CPACK_GENERATOR "TGZ")
+  endif()
+elseif ( WIN32 )
   # packaging needs WiX Tooset installed 
   set(CPACK_GENERATOR "WIX;ZIP")
   # Upgrade Code must always be the same, so the setup can replace the existing installation
