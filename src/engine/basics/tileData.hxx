537cae82f8d3b2936a834829b9d0ce1aa2688276
JimmySnails <talantyyr@gmail.com>
2020-03-07 09:13:35 +0100

    Replace TileType by an enum

--- src/engine/basics/tileData.hxx
+++ src/engine/basics/tileData.hxx
@@ -2,6 +2,7 @@
 #define TILEDATA_HXX_
 
 #include <string>
+#include "enums.hxx"
 
 /// min and max values for tileData
 #define TD_PRICE_MIN 0
@@ -64,7 +65,7 @@ struct TileData
   uint8_t buildingsize; /// the size that the building occupies in tiles.
   std::string biome;    /// the biome the tile belongs to
   TileSetData tiles;
-  std::string tileType; /// todo: String for now, should be an enum
+  TileType tileType = TileType::default; /// todo: String for now, should be an enum
   TileSetData cornerTiles;
   TileSetData slopeTiles;
   std::string title;
