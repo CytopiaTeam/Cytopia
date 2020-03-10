272959905968137840f6c3f81809a8d64965eccf
JimmySnails <talantyyr@gmail.com>
2020-03-10 13:42:12 +0100

    use target macOS since the TRAVIS env var don't seem to work

--- tools/CI/upload-CI-build.sh
+++ tools/CI/upload-CI-build.sh
@@ -4,17 +4,7 @@ set -eu
 
 #if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]
 #then
-  if [ "${TRAVIS_OS_NAME}" = "linux" ]
-  then
-    # Get butler
-    wget https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default -O butler.zip
-    7z x butler.zip
-    sudo chmod a+x ./butler
-  
-    # Setup redist folder and push it to itch
-    ninja install
-    ./butler push redist cytopia/cytopia:linux-ci --userversion 0.2-CIBuild-${TRAVIS_JOB_NUMBER}
-  elif [ "${TRAVIS_OS_NAME}" = "osx" ]
+  if [ "${TARGET}" = "macOS" ]
   then
     # Get butler
     curl -JOL https://broth.itch.ovh/butler/darwin-amd64/LATEST/archive/default
@@ -22,5 +12,14 @@ set -eu
     mv bin/Cytopia.app/ redist/
     unzip butler-darwin-amd64.zip
     ./butler push redist cytopia/cytopia:macos-ci --userversion 0.2-CIBuild-${TRAVIS_JOB_NUMBER}
+  else
+  # Get butler
+    wget https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default -O butler.zip
+    7z x butler.zip
+    sudo chmod a+x ./butler
+  
+    # Setup redist folder and push it to itch
+    ninja install
+    ./butler push redist cytopia/cytopia:linux-ci --userversion 0.2-CIBuild-${TRAVIS_JOB_NUMBER}
   fi
 #fi
