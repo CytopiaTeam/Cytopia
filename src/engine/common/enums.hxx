d03fb37aef134e821b6d73075d00a82bbb45b10e
JimmySnails <talantyyr@gmail.com>
2020-04-04 16:00:56 +0200

    add documentation and GROUND_DECORATION layer

--- src/engine/common/enums.hxx
+++ src/engine/common/enums.hxx
@@ -4,23 +4,25 @@
 #include <stddef.h>
 #include "betterEnums.hxx"
 
+/// All Layers we have
 enum Layer : unsigned int
 {
-  NONE,            // 0- this must be FIRST !!!
-  BLUEPRINT,       // 1- Optional layer - Map Blueprint
-  UNDERGROUND,     // 2- Optional layer - Pipes, Subway-pipes and so onn
-  TERRAIN,         // 3- Terrain tiles, decorations, ... - must always be a "full" tile
-  WATER,           // 4- Water tiles
-  MOVABLE_OBJECTS, // 5- Cars, Pedestrians
-  BUILDINGS,       // 6- Buildings, Streets and everything that goes on the terrain
-  ANIMATIONS,      // 7- Animations like smoke
-  SYMBOLS,         // 8- Symbols to display over buildings like no power / water and so on
-  LAYERS_COUNT     // this must be LAST !!!
+  NONE,              /// 0- this must be FIRST !!!
+  BLUEPRINT,         /// 1- Optional layer - Map Blueprint
+  UNDERGROUND,       /// 2- Optional layer - Pipes, Subway-pipes and so onn
+  TERRAIN,           /// 3- Terrain tiles, decorations, ... - must always be a "full" tile
+  WATER,             /// 4- Water tiles
+  MOVABLE_OBJECTS,   /// 5- Cars, Pedestrians
+  BUILDINGS,         /// 6- Buildings, Streets and everything that goes on the terrain
+  ANIMATIONS,        /// 7- Animations like smoke
+  SYMBOLS,           /// 8- Symbols to display over buildings like no power / water and so on
+  GROUND_DECORATION, /// 9- Decoration to place beneath buildings. Like concrete or grass
+  LAYERS_COUNT       /// this must be LAST !!!
 };
 
 constexpr size_t FIRST_LAYER = NONE + 1;
 
-/// This is a orderd list of all relevant layers we need to interact with
+/// This is a ordered list of all relevant layers we need to interact with
 static const Layer allLayersOrdered[] = {Layer::TERRAIN, Layer::WATER, Layer::BUILDINGS, Layer::BLUEPRINT, Layer::UNDERGROUND};
 
 /**
@@ -31,16 +33,16 @@ static const Layer allLayersOrdered[] = {Layer::TERRAIN, Layer::WATER, Layer::BU
  */
 enum class LayerEditMode
 {
-  TERRAIN,
-  BLUEPRINT
+  TERRAIN,  /// Default "overworld" edit mode
+  BLUEPRINT /// Placing water pipes and underground transportation on the Blueprint layer
 };
 
 enum class PlacementMode
 {
-  SINGLE,
-  STRAIGHT_LINE,
-  LINE,
-  RECTANGLE
+  SINGLE,        /// Place tiles on a single spot
+  STRAIGHT_LINE, /// Place tiles in a straight, rectangular line
+  LINE,          /// Place tiles in a line from start to end point
+  RECTANGLE      /// draw a rectangle between start and end point
 };
 
 #endif
