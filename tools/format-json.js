9e3f4e9a8ec6f3cd43674c01f1553398fc2721ee
Restyled.io <commits@restyled.io>
2020-04-28 08:08:30 +0000

    Restyled by clang-format

--- tools/format-json.js
+++ tools/format-json.js
@@ -2,25 +2,19 @@
 const fs = require("fs");
 
 const fileList = [
-    "../data/resources/settings.json",
-    "../data/resources/data/AudioConfig.json",
-    "../data/resources/data/TerrainGen.json",
-    "../data/resources/data/TileData.json",
-    "../data/resources/data/UIData.json",
-    "../data/resources/data/UILayout.json"
+  "../data/resources/settings.json", "../data/resources/data/AudioConfig.json", "../data/resources/data/TerrainGen.json",
+  "../data/resources/data/TileData.json", "../data/resources/data/UIData.json", "../data/resources/data/UILayout.json"
 ];
 
-function formatFile(file, index) {
-    fs.readFile(file, "utf8", (err, content) => {
-        if (err) throw err;
-        fs.writeFile(file, JSON.stringify( JSON.parse(content), null, 4), (err) => {
-            if (err) throw err;
-            console.log(`formatting file ${file} done`);
-        });
+function formatFile(file, index)
+{
+  fs.readFile(file, "utf8", (err, content) => {
+    if (err) throw err;
+    fs.writeFile(file, JSON.stringify(JSON.parse(content), null, 4), (err) => {
+      if (err) throw err;
+      console.log(`formatting file ${file} done`);
     });
+  });
 }
 
 fileList.forEach(formatFile);
-
-
-
