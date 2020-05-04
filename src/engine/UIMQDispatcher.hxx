fc0bba2d8c908a35da6c1f948f7ab180d4a399eb
Ercadio <vroch@edu.uwaterloo.ca>
2020-05-04 17:23:48 -0400

    Adding iModel and DataModel

---
+++ src/engine/UIMQDispatcher.hxx
@@ -0,0 +1,24 @@
+#ifndef UI_MQ_DISPATCHER
+#define UI_MQ_DISPATCHER
+
+#include "Observer.hxx"
+#include "MessageQueue.hxx"
+
+/**
+ * @class   UIMQDispatcher
+ * @brief   An asynchronous dispatch to Observers
+ *          based on a UIMessageQueue processed on another thread
+ * @details This dispatch policy allows for the GameLoop to asynchronously 
+ *          update the Views while modifying iModels
+ * @tparam  DataArgs The data arguments
+ */
+template <typename DataArgs> struct UIMQDispatcher
+{
+  class UILoopMQ &loop;
+  void dispatch(ObserverSPtr<DataArgs> observer, typename Observer<DataArgs>::Notification notification)
+  {
+    loop.push(UIChangeEvent{observer, std::move(notification)});
+  }
+};
+
+#endif // UI_MQ_DISPATCHER
