b472cbc265661357049a3f61c5e0e3fc18a08e19
JimmySnails <talantyyr@gmail.com>
2020-05-07 21:56:54 +0200

    add platforms/android dir for gradle builds

---
+++ platforms/android/android-app/src/main/java/com/my/exampleapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.my.exampleapp;
+
+
+public class MainActivity extends org.libsdl.app.SDLActivity {
+
+    /**
+     * This method is called by SDL before loading the native shared libraries.
+     * It can be overridden to provide names of shared libraries to be loaded.
+     * The default implementation returns the defaults. It never returns null.
+     * An array returned by a new implementation must at least contain "SDL2".
+     * Also keep in mind that the order the libraries are loaded may matter.
+     *
+     * @return names of shared libraries to be loaded (e.g. "SDL2", "main").
+     */
+    @Override
+    protected String[] getLibraries() {
+        return new String[]{
+                "SDL2",
+                "SimpleExample"
+        };
+    }
+
+}
