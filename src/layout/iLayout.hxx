98ba70cb768541046f7f9f8528239130cb56b352
Ercadio <vroch@edu.uwaterloo.ca>
2020-05-10 11:19:54 -0400

    Adding AbsoluteLayout

---
+++ src/layout/iLayout.hxx
@@ -0,0 +1,49 @@
+#ifndef I_LAYOUT_HXX
+#define I_LAYOUT_HXX
+
+#include <vector>
+
+#include "../util/Range.hxx"
+#include "../util/BoxSizing.hxx"
+#include "../view/iView.hxx"
+#include "../view/iViewElement.hxx"
+
+class iLayout : public virtual iView
+{
+
+  using ElementList = std::vector<iViewPtr>;
+  ElementList m_Elements;
+  Rectangle m_Bounds;
+  
+public:
+
+  virtual void draw(iRenderer &) const noexcept override;
+
+  iLayout(Rectangle && = Rectangle{0, 0, 0, 0});
+  virtual ~iLayout() = 0;
+  
+  void setup(GameService &) noexcept override;
+
+protected:
+
+  /**
+   * @brief Adds a iView to the iLayout
+   * @param iViewPtr the iView to add
+   */
+  iViewPtr addElement(iViewPtr);
+
+  Range<typename ElementList::iterator> elements() noexcept;
+  Range<typename ElementList::const_iterator> elements() const noexcept;
+
+  /**
+   * @brief compute the boundaries of all iViews
+   */
+  virtual void computeBoundaries() noexcept = 0;
+
+  const Rectangle & getBounds() const noexcept final;
+
+private:
+  void setBounds(Rectangle &&) noexcept final;
+};
+
+#endif // I_LAYOUT_HXX
---
+++ src/view/iViewElement.hxx
@@ -0,0 +1,19 @@
+#ifndef I_VIEW_ELEMENT_HXX
+#define I_VIEW_ELEMENT_HXX
+
+#include <memory>
+#include "iView.hxx"
+#include "../util/Rectangle.hxx"
+
+class iViewElement : public iView
+{
+  Rectangle m_Bounds = Rectangle{0, 0, 0, 0};
+public:
+  virtual ~iViewElement() = 0;
+  virtual const Rectangle & getBounds() const noexcept final;
+  void setBounds(Rectangle &&) noexcept override;
+};
+
+using iViewElementPtr = std::shared_ptr<iViewElement>;
+
+#endif // I_VIEW_ELEMENT_HXX
