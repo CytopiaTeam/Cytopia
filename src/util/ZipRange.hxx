89e840035a24647e15600d40f8afe796e714fcff
Restyled.io <commits@restyled.io>
2020-05-10 23:59:53 +0000

    Restyled by clang-format

--- src/util/ZipRange.hxx
+++ src/util/ZipRange.hxx
@@ -9,45 +9,39 @@
  * @brief     A Range based on zipping multiple Ranges
  * @example   tests/util/ZipRange.cxx
  */
-template <typename Iterator, typename... Iterators>
-class ZipRange
+template <typename Iterator, typename... Iterators> class ZipRange
 {
 public:
-
   class ZipIterator
   {
     using TupleIterator = std::tuple<Iterator, Iterators...>;
     TupleIterator m_It;
+
   public:
-    using value_type = std::tuple<
-      typename std::iterator_traits<Iterator>::value_type, 
-      typename std::iterator_traits<Iterators>::value_type ...>;
+    using value_type =
+        std::tuple<typename std::iterator_traits<Iterator>::value_type, typename std::iterator_traits<Iterators>::value_type...>;
     using difference_type = std::ptrdiff_t;
-    using pointer = std::tuple<
-      typename std::iterator_traits<Iterator>::pointer, 
-      typename std::iterator_traits<Iterators>::pointer ...>;
-    using reference = std::tuple<
-      typename std::iterator_traits<Iterator>::reference, 
-      typename std::iterator_traits<Iterators>::reference ...>;
+    using pointer =
+        std::tuple<typename std::iterator_traits<Iterator>::pointer, typename std::iterator_traits<Iterators>::pointer...>;
+    using reference =
+        std::tuple<typename std::iterator_traits<Iterator>::reference, typename std::iterator_traits<Iterators>::reference...>;
     using iterator_category = std::forward_iterator_tag;
-    ZipIterator(Iterator, Iterators ...);
-    bool operator==(const ZipIterator & other) const;
-    bool operator!=(const ZipIterator & other) const;
-    ZipIterator & operator++();
+    ZipIterator(Iterator, Iterators...);
+    bool operator==(const ZipIterator &other) const;
+    bool operator!=(const ZipIterator &other) const;
+    ZipIterator &operator++();
     ZipIterator operator++(int);
     reference operator*();
   };
 
   ZipRange() = delete;
 
-  template <typename Range, typename... Ranges>
-  explicit ZipRange(Range &&, Ranges && ...);
-  
+  template <typename Range, typename... Ranges> explicit ZipRange(Range &&, Ranges &&...);
+
   /**
    * @brief   Do not use. Will issue an error
    */
-  template <typename Range, typename... Ranges>
-  explicit ZipRange(const Range &&, const Ranges && ...);
+  template <typename Range, typename... Ranges> explicit ZipRange(const Range &&, const Ranges &&...);
 
   ZipIterator begin() const;
   ZipIterator end() const;
@@ -59,7 +53,6 @@ public:
   using pointer = typename ZipIterator::pointer;
   using const_pointer = const typename ZipIterator::pointer;
   using iterator = ZipIterator;
-
 
 private:
   ZipIterator m_Begin;
--- tests/util/ZipRange.cxx
+++ tests/util/ZipRange.cxx
@@ -6,11 +6,11 @@
 
 TEST_CASE("I can use ZipRanges", "[util]")
 {
-  const std::array myInts { 0, 1, 2, 3, 4 };
-  const std::vector myChars { '0', '1', '2', '3', '4' };
-  const std::initializer_list<std::string> myStrings { "0", "1", "2", "3", "4" };
+  const std::array myInts{0, 1, 2, 3, 4};
+  const std::vector myChars{'0', '1', '2', '3', '4'};
+  const std::initializer_list<std::string> myStrings{"0", "1", "2", "3", "4"};
   int count = 0;
-  for(const auto & [i, c, s] : ZipRange{myInts, myChars, myStrings})
+  for (const auto &[i, c, s] : ZipRange{myInts, myChars, myStrings})
   {
     REQUIRE(std::to_string(i) == s);
     REQUIRE(std::string(1, c) == s);
@@ -22,13 +22,13 @@ TEST_CASE("I can use ZipRanges", "[util]")
 TEST_CASE("ZipRanges will not go past std::end", "[util]")
 {
   /* Consider a ZipRange of containers of different lengths... */
-  const std::array myInts { 0, 1, 2, 3 };
+  const std::array myInts{0, 1, 2, 3};
   const std::vector<int> empty;
-  const ZipRange range { myInts, empty };
+  const ZipRange range{myInts, empty};
   REQUIRE(range.begin() == range.end());
   REQUIRE(!(range.begin() != range.end()));
   int count = 0;
-  for (const auto & [i1, i2] : range)
+  for (const auto &[i1, i2] : range)
   {
     ++count;
   }
