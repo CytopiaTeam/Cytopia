ef12693e9c0846cc8bd7411cd296eef0161d3636
Restyled.io <commits@restyled.io>
2020-05-13 20:02:08 +0000

    Restyled by clang-format

--- src/util/Color.hxx
+++ src/util/Color.hxx
@@ -9,7 +9,7 @@ class SDL_Color;
 struct RGBAColor
 {
 public:
-  constexpr RGBAColor(uint32_t color) : m_Color(color) { }
+  constexpr RGBAColor(uint32_t color) : m_Color(color) {}
   int hue() const noexcept;
   int lightness() const noexcept;
   int saturation() const noexcept;
@@ -18,26 +18,22 @@ public:
   uint8_t blue() const noexcept;
   uint8_t alpha() const noexcept;
 
-  [[deprecated("Use sdl::toColor instead")]]
-  SDL_Color to_SDL() const noexcept;
+  [[deprecated("Use sdl::toColor instead")]] SDL_Color to_SDL() const noexcept;
 
 private:
   uint32_t m_Color;
 };
 
-constexpr RGBAColor operator""_rgba(unsigned long long val)
-{
-  return RGBAColor{static_cast<uint32_t>(val)};
-}
+constexpr RGBAColor operator""_rgba(unsigned long long val) { return RGBAColor{static_cast<uint32_t>(val)}; }
 
-std::ostream & operator<<(std::ostream &, const RGBAColor &);
+std::ostream &operator<<(std::ostream &, const RGBAColor &);
 
 namespace Palette
 {
-  constexpr RGBAColor Gray = 0x575757FF_rgba;
-  constexpr RGBAColor Orange = 0xFF771EFF_rgba;
-  constexpr RGBAColor Black = 0x000000FF_rgba;
-  constexpr RGBAColor White = 0xFFFFFFFF_rgba;
-};
+constexpr RGBAColor Gray = 0x575757FF_rgba;
+constexpr RGBAColor Orange = 0xFF771EFF_rgba;
+constexpr RGBAColor Black = 0x000000FF_rgba;
+constexpr RGBAColor White = 0xFFFFFFFF_rgba;
+}; // namespace Palette
 
 #endif // COLOR_HXX_
--- tests/util/BoxSizing.cxx
+++ tests/util/BoxSizing.cxx
@@ -8,20 +8,20 @@ TEST_CASE("I can use ScreenMeasurement", "[util]")
   REQUIRE((1_eh).eh == 1);
   REQUIRE((1_ew).ew == 1);
   REQUIRE((1_px).px == 1);
-  
+
   REQUIRE((1_lh + 1_lh).lh == 2);
   REQUIRE((1_lw + 1_lw).lw == 2);
   REQUIRE((1_eh + 1_eh).eh == 2);
   REQUIRE((1_ew + 1_ew).ew == 2);
   REQUIRE((1_px + 1_px).px == 2);
-  
+
   REQUIRE((2_lh - 1_lh).lh == 1);
   REQUIRE((2_lw - 1_lw).lw == 1);
   REQUIRE((2_eh - 1_eh).eh == 1);
   REQUIRE((2_ew - 1_ew).ew == 1);
   REQUIRE((2_px - 1_px).px == 1);
 
-  ScreenMeasurementValuation smv { 100, 200, 300, 400 };
+  ScreenMeasurementValuation smv{100, 200, 300, 400};
   REQUIRE((1_ew).measure(smv) == 1);
   REQUIRE((1_eh).measure(smv) == 2);
   REQUIRE((1_lw).measure(smv) == 3);
@@ -29,4 +29,3 @@ TEST_CASE("I can use ScreenMeasurement", "[util]")
   REQUIRE((5_px).measure(smv) == 5);
   REQUIRE((1_ew + 1_eh + 1_lw + 1_lh + 5_px).measure(smv) == 15);
 }
-
