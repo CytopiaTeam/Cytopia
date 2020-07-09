0f1e6c62906633d17659b0b96a393f08ec98ee0d
Restyled.io <commits@restyled.io>
2020-07-09 06:49:07 +0000

    Restyled by shellharden

--- tools/CI/upload-CI-apk.sh
+++ tools/CI/upload-CI-apk.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]
 then
   # Get butler
     wget https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default -O butler.zip
@@ -10,5 +10,5 @@ then
     sudo chmod a+x ./butler
 
     # Setup redist folder and push it to itch
-    ./butler push ./platforms/android/build/Cytopia/outputs/apk/debug/Cytopia-debug.apk cytopia/cytopia:android-ci --userversion 0.2-CIBuild-${TRAVIS_JOB_NUMBER}
+    ./butler push ./platforms/android/build/Cytopia/outputs/apk/debug/Cytopia-debug.apk cytopia/cytopia:android-ci --userversion 0.2-CIBuild-"$TRAVIS_JOB_NUMBER"
 fi
