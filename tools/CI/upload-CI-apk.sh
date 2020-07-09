030de5acdd0f3431d0435bfb4eb654d3e384af62
Restyled.io <commits@restyled.io>
2020-07-09 06:49:06 +0000

    Restyled by whitespace

--- tools/CI/upload-CI-apk.sh
+++ tools/CI/upload-CI-apk.sh
@@ -8,7 +8,7 @@ then
     wget https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default -O butler.zip
     7z x butler.zip
     sudo chmod a+x ./butler
-  
+
     # Setup redist folder and push it to itch
     ./butler push ./platforms/android/build/Cytopia/outputs/apk/debug/Cytopia-debug.apk cytopia/cytopia:android-ci --userversion 0.2-CIBuild-${TRAVIS_JOB_NUMBER}
 fi
