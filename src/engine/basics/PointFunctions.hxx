bfabe0309892115117d7df5d0753ec1ecfb685ca
Lisa Welsch <lisa.welsch1985@gmail.com>
2022-03-24 08:55:12 +0100

    fix documentation

--- src/engine/basics/PointFunctions.hxx
+++ src/engine/basics/PointFunctions.hxx
@@ -14,24 +14,24 @@ class PointFunctions
 public:
   /** \brief Creates a line between two points using the Bresenham Line algorithm
 * Creates a line between two points using the Bresenham Line algorithm
-* @param Point() - start coordinates
-* @param Point() - end coordinates
+* @param isoCoordinatesStart - start coordinates
+* @param isoCoordinatesEnd - end coordinates
 * @return std::vector<Point>() - contains coordinates for each tile between start and end coordinates, including start and end
 */
-  static std::vector<Point> getLine(Point isoCoordinatesStart, Point end);
+  static std::vector<Point> getLine(Point isoCoordinatesStart, Point isoCoordinatesEnd);
 
   /** \brief Gets all nodes in a straight line from start and end point
 * Gets all nodes in a straight line between start and end point
-* @param Point() - start coordinates
-* @param Point() - end coordinates
+* @param isoCoordinatesStart - start coordinates
+* @param isoCoordinatesEnd - end coordinates
 * @return std::vector<Point>() - contains coordinates for each tile between start and end coordinates, including start and end
 */
   static std::vector<Point> getStraightLine(const Point &isoCoordinatesStart, const Point &isoCoordinatesEnd);
 
   /** \brief Gets all nodes in a rectangular area between start and end point
 * Gets all nodes in a rectangle between start and end point
-* @param Point() - start coordinates
-* @param Point() - end coordinates
+* @param isoCoordinatesStart - start coordinates
+* @param isoCoordinatesEnd - end coordinates
 * @return std::vector<Point>() - contains coordinates for each tile between start and end coordinates, including start and end
 */
   static std::vector<Point> getArea(const Point &isoCoordinatesStart, const Point &isoCoordinatesEnd);
