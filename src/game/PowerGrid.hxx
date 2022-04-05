7275b7f009b92ea66cb9eaed87eef125dd40e652
Lisa Welsch <lisa.welsch1985@gmail.com>
2022-04-05 19:22:27 +0200

    add powerplant struct

--- src/game/PowerGrid.hxx
+++ src/game/PowerGrid.hxx
@@ -2,12 +2,22 @@
 #define POWERGRID_HXX_
 
 #include "point.hxx"
+#include <vector>
+
+struct PowerPlant
+{
+  Point coordinate;
+  int powerProduction = 0;
+};
 
 class PowerGrid
 {
 public:
   PowerGrid();
 
+private:
+  std::vector<PowerPlant> m_powerPlants;
+  std::vector<Point> doesnotwork;
 };
 
 #endif
