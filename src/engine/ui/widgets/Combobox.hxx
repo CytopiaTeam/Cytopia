7cf562d42f0e6abc3247663d3581297f3b081573
Elizabeth Sherrock <lizzyd710@gmail.com>
2022-04-29 03:15:57 -0400

    Refactored DropdownMenu into ComboBox (#885)
    * Renamed TextField to DropdownMenu
    
    * Deleted TextField after rename complete
    
    * Renamed some fields and methods for clarity
    
    * Added documentation to member fields and functions
    
    * Commented out quick duplicate/redundant code
    
    * Fully refactored DropdownMenu into Combobox (1/2)
    
    * Deleted DropdownMenu widget (2/2)
    
    * Minor changes from code review

--- src/engine/ui/widgets/Combobox.hxx
+++ src/engine/ui/widgets/Combobox.hxx
@@ -4,7 +4,15 @@
 #include <memory>
 
 #include "../basics/UIElement.hxx"
-#include "TextField.hxx"
+#include "Text.hxx"
+
+
+enum class TextFieldAlignment
+{
+  LEFT,
+  RIGHT,
+  CENTERED
+};
 
 class ComboBox : public UIElement
 {
@@ -31,35 +39,48 @@ public:
   * @return selected ID
   */
   int getActiveID() const { return m_activeID; };
+
   /** @brief Set ID of selected element
   * sets the ID of the selected element in the comboBox
   * @param ID the value to set the ID to
   */
   void setActiveID(int ID);
 
-  size_t count() const { return m_textField->count(); };
+  size_t count() const { return m_items.size(); };
+
+  /// the text displayed in the Combobox
   std::string activeText;
 
+  int hoveredID = -1;
+
+  TextFieldAlignment textAlignment = TextFieldAlignment::CENTERED;
   void registerCallbackFunction(std::function<void(UIElement *sender)> const &cb) override;
 
-  /** @brief Clears the element's text field
-  * clears the text field of the comboBox
+  /** @brief Clears the element's items
+  * clears the items in the dropdown menu of the comboBox
   */
-  void clear() { m_textField->clear(); }
+  void clear() { m_items.clear(); }
+
+  std::string getTextFromID(int id) const;
 
 private:
   /// the active element's ID
   int m_activeID = 0;
   /// represents the dropdownMenu
-  SDL_Rect m_dropDownRect;
+  SDL_Rect m_dropdownRect;
   /// represents the whole UIElement including the opened menu
   SDL_Rect m_wholeElementRect;
-  /// pointer to the textField of this comboBox
-  std::unique_ptr<TextField> m_textField;
+  // a rect is drawn beneath the current text to hover it
+  SDL_Rect m_highlightingRect;
+
+  /// the items in the dropdown menu as Text widgets
+  std::vector<std::unique_ptr<Text>> m_items;
+  /// the current item as a Text widget
+  std::unique_ptr<Text> m_selectedItem;
+  /// height of each item
+  int m_itemHeight = 0;
 
   bool m_isMenuOpened = false;
-
-  std::unique_ptr<Text> m_buttonLabel;
 
   void centerTextLabel() const;
 
