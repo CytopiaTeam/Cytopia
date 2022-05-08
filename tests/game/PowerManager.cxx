d3b36550d98b967d17436b5a7cff567f8baa20d8
Lisa Welsch <lisa.welsch1985@gmail.com>
2022-05-08 20:56:18 +0200

    add unfinished unittest for power manager

---
+++ tests/game/PowerManager.cxx
@@ -0,0 +1,30 @@
+#include <catch.hpp>
+#include "../../src/engine/common/Constants.hxx"
+#include "../../src/engine/Map.hxx"
+#include "../../src/game/PowerManager.hxx"
+#include "../../src/game/ZoneManager.hxx"
+
+#define private public
+#define protected public
+
+#define class struct
+
+TEST_CASE("Zone can be powered", "[powermanager][powermanager]")
+{
+  Map *map = new Map(64, 64, false);
+  PowerManager powerManager;
+  ZoneManager zoneManager;
+  map->setTileID("pow_5x5_Kohlekraftwerk_Durnrohr_FN", Point{20, 20});
+  for (int x = 21; x <= 30; x++)
+  {
+    map->setTileID("pow_RuralPowerLines", Point{x, 20});
+  }
+  for (int x = 31; x <= 40; x++)
+    for (int y = 20; y <= 30; y++)
+    {
+      map->setTileID("zone_residential_dense", Point{x, y});
+    }
+  // i don't know what to check for. all functions on managers are private and i don't want to
+  // make functions public for no other purpose than unittests
+  CHECK(map != nullptr);
+}
