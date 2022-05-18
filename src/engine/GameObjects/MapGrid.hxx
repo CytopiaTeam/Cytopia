35f0aa28c76655dd22309245c117ad6bf4a39997
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2022-05-19 02:45:52 +0800

    MapGrid base class (#922)
    MapGrid.hxx and MapGrid.inl.hxx
    
    PowerGrid and ZoneArea derived from MapGrid
    
    Move same function in PowerGrid and ZoneArea to MapGrid

---
+++ src/engine/GameObjects/MapGrid.hxx
@@ -0,0 +1,56 @@
+#ifndef MAPGRID_HXX_
+#define MAPGRID_HXX_
+
+#include "../basics/point.hxx"
+#include <vector>
+
+template <class T> class MapGrid
+{
+public:
+  MapGrid(T node);
+  ~MapGrid();
+
+  size_t size() { return m_gridNodes.size(); };
+
+  /**
+   * @bried Add a T node to this gridnode
+   *
+   * @node node to add
+   */
+  virtual void addNode(T node);
+
+  /**
+   * @bried Remove a node on a given coordinate from this gridnode
+   *
+   * @node coordinate of the node
+   */
+  virtual void removeNode(Point coordinate);
+
+  /**
+   * @brief If this coordinate is a neighbor of one of the node in the grid.
+   *
+   * @param coordinate the point to check
+   * @return if the given coordinate is a neighbor of this grid
+   */
+  bool isNeighbor(Point coordinate) const;
+
+  /**
+   * @brief If this coordinate is part of the grid.
+   *
+   * @param coordinate the point to check
+   * @return if the given coordinate is a member of this grid
+   */
+  bool isMemberOf(Point coordinate) const;
+
+  // iterators
+  auto begin() { return m_gridNodes.begin(); }
+  auto end() { return m_gridNodes.end(); }
+
+protected:
+  std::vector<T> m_gridNodes;
+};
+
+#include "MapGrid.inl.hxx"
+
+#endif
+
---
+++ src/engine/GameObjects/MapGrid.inl.hxx
@@ -0,0 +1,47 @@
+template<class T>
+MapGrid<T>::MapGrid(T node) : m_gridNodes{node} {}
+
+template<class T>
+MapGrid<T>::~MapGrid()
+{
+  m_gridNodes.clear();
+}
+
+template<class T>
+void MapGrid<T>::addNode(T node)
+{
+  m_gridNodes.push_back(node);
+}
+
+template<class T>
+void MapGrid<T>::removeNode(Point coordinate)
+{
+  m_gridNodes.erase(std::remove_if(m_gridNodes.begin(), m_gridNodes.end(),
+        [coordinate](const T &node) {
+        return node.coordinate == coordinate;
+        }),
+      m_gridNodes.end());
+}
+
+template<class T>
+bool MapGrid<T>::isNeighbor(Point coordinate) const
+{
+  for (const T &node : m_gridNodes)
+  {
+    if (node.coordinate.isDirectNeighborOf(coordinate))
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+template<class T>
+bool MapGrid<T>::isMemberOf(Point coordinate) const
+{
+  return m_gridNodes.end() != std::find_if(m_gridNodes.begin(), m_gridNodes.end(),
+      [&coordinate](const T &node) {
+      return node.coordinate == coordinate;
+      });
+}
+
