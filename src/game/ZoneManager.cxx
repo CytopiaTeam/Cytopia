575e4b8c8aa8b275977927ef75aabaf651e1dcda
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2022-05-24 20:59:26 +0800

    :fix: small codesmells issues

--- src/game/ZoneManager.cxx
+++ src/game/ZoneManager.cxx
@@ -198,20 +198,18 @@ void ZoneManager::updatePower(const std::vector<PowerGrid> &powerGrid)
 {
   for (const auto &grid : powerGrid)
   {
-    bool isGridConnected = false;
     for (auto &area : m_zoneAreas)
     {
-      isGridConnected = area.end() != std::find_if(area.begin(), area.end(),
+      bool isGridConnected = area.end() != std::find_if(area.begin(), area.end(),
           [grid](const ZoneNode &node) { return grid.isNeighbor(node.coordinate); });
-      if (isGridConnected) {
-        if(grid.getPowerLevel() > 0)
-        {
-          area.setPowerSupply(true);
-        }
-        else
-        {
-          area.setPowerSupply(false);
-        }
+      if (!isGridConnected)
+        continue;
+      if (grid.getPowerLevel() > 0)
+      {
+        area.setPowerSupply(true);
+      } else
+      {
+        area.setPowerSupply(false);
       }
     }
   }
