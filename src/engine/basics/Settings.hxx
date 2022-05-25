4f7db5453f124006c17dc099df71b70fd14740bc
dalerank <dalerankn8@gmail.com>
2022-05-25 13:12:04 +0200

    ui: config: read fontsize from settings.json (#936)
    * ui: config: read fontsize from settings.json
    
    task: https://github.com/CytopiaTeam/Cytopia/issues/788
    
    * ui: config: read fontsize from settings.json
    
    task: https://github.com/CytopiaTeam/Cytopia/issues/788
    
    * ui: config: read fontsize from settings.json, default in settings.json
    
    task: https://github.com/CytopiaTeam/Cytopia/issues/788
    
    * Fixed error in json
    
    Co-authored-by: s.kushnirenko <s.kushnirenko@gaijin.ru>

--- src/engine/basics/Settings.hxx
+++ src/engine/basics/Settings.hxx
@@ -159,6 +159,8 @@ struct SettingsData
    */
   int subMenuButtonHeight;
 
+  uint32_t defaultFontSize;
+
   /// indicates whether we want to see buildings inside Blueprint layer or not
   bool showBuildingsInBlueprint;
 
