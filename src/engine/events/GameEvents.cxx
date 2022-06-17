5feb5d8e9defcd9b4f1a959fa2c934b9a23c9444
Lisa Welsch <lisa.welsch1985@gmail.com>
2022-06-17 11:17:43 +0200

    fixes

--- src/engine/events/GameEvents.cxx
+++ src/engine/events/GameEvents.cxx
@@ -1,6 +1,8 @@
 #include "GameEvents.hxx"
 #include <MapFunctions.hxx>
 #include <Camera.hxx>
+#include "../basics/isoMath.hxx"
+
 #include <LOG.hxx>
 
 void GameEvents::unHighlightNodes()
@@ -22,19 +24,31 @@ void GameEvents::unHighlightNodes()
 
 void GameEvents::processEvents(SDL_Event &event)
 {
+  if (!MapFunctions::instance().getMap())
+  { // abort if there's no map to interact with
+    return;
+  }
+
+  SDL_Point mouseScreenCoords = {event.button.x, event.button.y};
+  Point mouseIsoCoords = convertScreenToIsoCoordinates(mouseScreenCoords);
+
   switch (event.type)
   {
     // ----- MOUSE MOTION
   case SDL_MOUSEMOTION:
     if (mouseDownRight)
-    {
+    { // if the right mouse button is being held down, pan the map
       if ((event.motion.xrel == 0) && (event.motion.yrel == 0))
       {
         return;
       }
       Camera::instance().moveCamera(event.motion.xrel, event.motion.yrel);
-      break;
+      break; // when we're panning, don't handle other events
     }
+
+    // clear highlighting, we'll rebuild it now
+    unHighlightNodes();
+
     break;
 
     // ----- MOUSEBUTTON DOWN
@@ -59,7 +73,7 @@ void GameEvents::processEvents(SDL_Event &event)
       mouseDownLeft = true;
       break;
     }
-
+    break;
   case SDL_MOUSEWHEEL:
     if (event.wheel.y > 0)
     {
