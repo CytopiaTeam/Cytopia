CytopiaTeam/Cytopia

commit d1e9691307185c82c8b842d085933f41a256b822

:wrench: Update conan generator to CMakeToolchain

src/engine/map/TerrainGenerator.cxx
@@ -9,11 +9,8 @@
 
 #include "json.hxx"
 
-#ifdef NOISE_IN_SUBDIR
 #include <noise/noise.h>
-#else
-#include <noise.h>
-#endif
+
 #include <random>
 
 using json = nlohmann::json;

.github/workflows/build-game.yml
@@ -19,7 +19,7 @@ jobs:
       - name: Cache conan
         uses: actions/cache@v3.2.4
         with:
-          key: conan-win-${{ hashFiles('cmake/DependenciesConfig.cmake') }}
+          key: conan-win-${{ hashFiles('conanfile.txt') }}
           path: ~/.conan/
 
       - name: Install dependencies
@@ -30,8 +30,10 @@ jobs:
 
       - name: Build
         run: |
+          conan config set general.revisions_enabled=1
           conan config set storage.download_cache="$env:TMP"
-          cmake . -GNinja -DCMAKE_BUILD_TYPE=Release
+          conan install . -b missing -pr:b=tools/conan-profiles/vs-22-release-ninja -pr=tools/conan-profiles/vs-22-release-ninja
+          cmake . -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"
           ninja
 
       - name: Test
@@ -63,7 +65,7 @@ jobs:
       - name: Cache conan
         uses: actions/cache@v3.2.4
         with:
-          key: conan-linux-${{ hashFiles('cmake/DependenciesConfig.cmake') }}
+          key: conan-linux-${{ hashFiles('conanfile.txt') }}
           path: ~/.conan/
 
       - name: Install dependencies
@@ -109,7 +111,14 @@ jobs:
 
       - name: Configure
         run: |
-          cmake . -GNinja -DCMAKE_BUILD_TYPE=Release -DCYTOPIA_USE_ANGELSCRIPT=CONAN
+          conan config set general.revisions_enabled=1
+          echo "tools.system.package_manager:mode = install" > ~/.conan/global.conf
+          echo "tools.system.package_manager:sudo = True" >> ~/.conan/global.conf
+          conan profile new --detect --force default
+          conan profile update conf.tools.cmake.cmaketoolchain:generator="Ninja" default
+          conan profile update settings.compiler.libcxx=libstdc++11 default
+          conan install . -s build_type=Release -b missing -pr:b=default
+          cmake . -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"
 
       - name: Build
         run: |
@@ -144,7 +153,7 @@ jobs:
       - name: Cache conan
         uses: actions/cache@v3.2.4
         with:
-          key: conan-mac-${{ hashFiles('cmake/DependenciesConfig.cmake') }}
+          key: conan-mac-${{ hashFiles('conanfile.txt') }}
           path: ~/.conan/
 
       - name: Install dependencies
@@ -152,7 +161,11 @@ jobs:
 
       - name: Build
         run: |
-          cmake . -GNinja -DCMAKE_BUILD_TYPE=Release -DCYTOPIA_USE_ANGELSCRIPT=CONAN
+          conan config set general.revisions_enabled=1
+          conan profile new --detect --force default
+          conan profile update conf.tools.cmake.cmaketoolchain:generator="Ninja" default
+          conan install . -s build_type=Release -b missing -pr:b=default
+          cmake . -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"
           ninja
           mkdir -p bin/Cytopia.App/Resources/resources
           cp -pR data/resources bin/Cytopia.App/Contents/Resources/

.github/workflows/sonarcloud.yml
@@ -44,7 +44,7 @@ jobs:
       - name: Cache conan
         uses: actions/cache@v3.2.4
         with:
-          key: conan-linux-${{ hashFiles('cmake/DependenciesConfig.cmake') }}
+          key: conan-linux-${{ hashFiles('conanfile.txt') }}
           path: ~/.conan/
 
       - name: Install dependencies
@@ -90,7 +90,14 @@ jobs:
 
       - name: Configure
         run: |
-          cmake . -DCMAKE_BUILD_TYPE=Release -DCYTOPIA_LIB_PREFERENCE=CONAN -DCYTOPIA_USE_MICROPROFILE=CONAN -DCYTOPIA_USE_ANGELSCRIPT=CONAN -DBUILD_TEST=ON -DENABLE_COVERAGE=ON
+          conan config set general.revisions_enabled=1
+          echo "tools.system.package_manager:mode = install" > ~/.conan/global.conf
+          echo "tools.system.package_manager:sudo = True" >> ~/.conan/global.conf
+          conan profile new --detect --force default
+          conan profile update conf.tools.cmake.cmaketoolchain:generator="Ninja" default
+          conan profile update settings.compiler.libcxx=libstdc++11 default
+          conan install . -s build_type=Release -b missing -pr:b=default
+          cmake . -DCMAKE_BUILD_TYPE=Release -DBUILD_TEST=ON -DENABLE_COVERAGE=ON -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"
 
       - name: Run build-wrapper
         run: build-wrapper-linux-x86-64 --out-dir bw-output make -j4

.gitignore
@@ -22,13 +22,9 @@ TileDataEditor
 
 # ignore macOS stuff.
 .DS_Store
-
 _pmm
-conanfile\.txt
 Doxygen_Docs/*
-
 *.mo
-
 tools/l10n/TMP
 
 #ignore redist directory
@@ -50,6 +46,5 @@ src/cmake_install.cmake
 src/Makefile
 
 tools/l10n/TMP.pot
-
 _build/
 /build*/

CMakeLists.txt
@@ -1,18 +1,13 @@
 cmake_minimum_required(VERSION 3.16 FATAL_ERROR)
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake" "${CMAKE_SOURCE_DIR}/cmake/find-modules")
+
+cmake_policy(SET CMP0127 OLD)
+
 include(CMakeDependentOption)
 include(Macros)
 include(FeatureSummary)
 include(SourceFileUtils)
 include(CMakeCM)
-set(_PREFIX "CYTOPIA_")
-
-set(CMAKE_CONFIGURATION_TYPES "Debug" "Release" CACHE STRING "Configuration types")
-
-# Test if conan is installed
-find_program(CONAN_CMD_TEST conan NO_CACHE)
-cmake_dependent_option(USE_PACKAGE_MANAGER "Use conan" ON "NOT CONAN_CMD_TEST STREQUAL CONAN_CMD_TEST-NOTFOUND" OFF)
-message(STATUS "Using conan: ${USE_PACKAGE_MANAGER}")
 
 # create the cmake project
 project(
@@ -28,17 +23,6 @@ project(
 
 
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
-
-# Allow user to globally set the library preference for external libraries
-cmd_option(
-        ${_PREFIX}LIB_PREFERENCE
-        "Library preference [SYSTEM (if available), CONAN]"
-        "CONAN"
-        STRINGS "SYSTEM" "CONAN"
-)
-
-# Special mode, that will force dependencies to the packages provided by system unless they were set to conan explicitly.
-option(${_PREFIX}FORCE_SYSTEM_DEPENDENCIES "Force the use of system packages")
 
 option(BUILD_TEST "Build Cytopia Tests" ON)
 option(ENABLE_PHC "Use a Precompiled header for speeding up the build" ON)
@@ -73,14 +57,7 @@ endif ()
 ################################################################################
 # Check for dependencies
 ################################################################################
-
-if (ANDROID)
-    message(STATUS "Crossbuilding to Android")
-else ()
-    include(DependenciesConfig)
-    resolve_conan_dependencies()
-endif ()
-
+include(DependenciesConfig)
 
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
     link_libraries(dbghelp.lib)
@@ -156,3 +133,6 @@ if (WIN32)
                 )
     endif ()
 endif ()
+
+
+feature_summary(WHAT ALL)

cmake/DependenciesConfig.cmake
@@ -1,77 +1,17 @@
-include(DependenciesFunctions)
+
 find_package(Threads REQUIRED)
+find_package(SDL2 REQUIRED)
+find_package(SDL2_image REQUIRED)
+find_package(SDL2_ttf REQUIRED)
+find_package(LibNoise REQUIRED)
+find_package(PNG REQUIRED)
 
-add_external_lib(
-        SDL2
-        sdl/2.26.0
-        REQUIRED
-        INTERFACE_NAME SDL::SDL
-        PKG_CONFIG "sdl2 >= 2.0"
-)
-add_external_lib(
-        SDL2_image
-        sdl_image/2.0.5
-        REQUIRED
-        INTERFACE_NAME SDL::SDL_image
-        PKG_CONFIG "SDL2_image"
-)
-add_external_lib(
-        SDL2_ttf
-        sdl_ttf/2.0.18
-        REQUIRED
-        INTERFACE_NAME SDL::TTF
-        PKG_CONFIG "SDL2_ttf"
-)
+find_package(OpenAL CONFIG)
+find_package(Vorbis)
+cmake_dependent_option(ENABLE_AUDIO "use OPENAL" ON "OPENAL_FOUND AND VORBISFILE_FOUND" OFF)
 
-add_external_lib(
-        ZLIB
-        zlib/1.2.13
-        REQUIRED
-        PKG_CONFIG "zlib"
-)
-add_external_lib(
-        LibNoise
-        libnoise/1.0.0
-        REQUIRED
-        FIND_PACKAGE
-)
-add_external_lib(
-        LIBPNG
-        libpng/1.6.39
-        REQUIRED
-        PKG_CONFIG "libpng"
-)
+find_package(Angelscript)
+cmake_dependent_option(USE_ANGELSCRIPT "use Angelscript" ON "TARGET Angelscript::angelscript" OFF)
 
-add_external_lib(
-        OpenAL
-        openal/1.22.2
-        PKG_CONFIG "openal >= 1.18"
-        FIND_PACKAGE_OPTIONS CONFIG
-)
-
-add_external_lib(
-        VorbisFile
-        vorbis/1.3.7
-        PKG_CONFIG "vorbis >= 1.2, vorbisfile >= 1.2"
-        FIND_PACKAGE
-)
-
-add_external_lib(
-        Angelscript
-        angelscript/2.35.1
-        FIND_PACKAGE
-        BY_DEFAULT_DISABLED
-)
-add_external_lib(
-        Microprofile
-        microprofile/3.1
-        FIND_PACKAGE
-        BY_DEFAULT_DISABLED
-)
-
-if (UNIX AND NOT APPLE)
-        add_external_lib(
-                libalsa
-                libalsa/1.2.7.2
-        )
-endif ()
+find_package(microprofile)
+option(USE_MICROPROFILE "use Microprofile" OFF)

cmake/DependenciesFunctions.cmake
@@ -1,297 +1,0 @@
-# Load Conan
-include(conan)
-include(FindPkgConfig)
-
-set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH TRUE)
-
-set(CONAN_BUILD_REQUIRES)
-set(CONAN_REQUIRES)
-set(CONAN_PACKAGE_OPTIONS)
-set(CONAN_ONLY_DEBUG_RELEASE)
-set(CONAN_CONFIG_OPTIONS)
-set(CONAN_RESOLVE_LIST)
-
-# Add a external dependency
-# Example usage:
-# add_external_lib( 
-#   OGRE
-#   ogre/1.12
-#   OPTION_NAME ogre
-#   SYMBOL OGRE
-#   REQUIRED
-#   FIND_PACKAGE
-#   ALWAYS_ALLOW_CONAN_FALLBACK
-#   PKG_CONFIG "ogre >= 1.11"
-#   FIND_PACKAGE_OPTIONS COMPONENTS Bites Overlay Paging RTShaderSystem MeshLodGenerator Terrain
-#   INTERFACE_NAME ogre::ogre
-#   HAS_ONLY_DEBUG_RELEASE
-#   CONAN_OPTIONS 
-#        ogre:shared=True
-# )
-
-function(add_external_lib package conan_package_name)
-    list(REMOVE_AT ARGV 0 1)
-    better_parse_args(
-            . REQUIRED FIND_PACKAGE ALWAYS_ALLOW_CONAN_FALLBACK HAS_ONLY_DEBUG_RELEASE BY_DEFAULT_DISABLED
-            - OPTION_NAME SYMBOL INTERFACE_NAME CONAN_PKG_NAME
-            + CONAN_OPTIONS FIND_PACKAGE_OPTIONS PKG_CONFIG
-    )
-
-    if (ARG_OPTION_NAME)
-        set(option_name_base ${ARG_OPTION_NAME})
-    else ()
-        string(TOUPPER "${package}" option_name_base)
-    endif ()
-
-    if (ARG_ALWAYS_ALLOW_CONAN_FALLBACK)
-        set(system_only OFF)
-    else ()
-        set(system_only ${${_PREFIX}FORCE_SYSTEM_DEPENDENCIES})
-    endif ()
-
-    if (ARG_FIND_PACKAGE_OPTIONS)
-        set(ARG_FIND_PACKAGE ON)
-    endif ()
-
-    if (ARG_INTERFACE_NAME)
-        set(interface_name ${ARG_INTERFACE_NAME})
-    else ()
-        set(interface_name "${package}::${package}")
-    endif ()
-
-    if (ARG_SYMBOL)
-        set(symbol ${ARG_SYMBOL})
-    else ()
-        set(symbol ${option_name_base})
-    endif ()
-
-    # Generate CMake option
-    set(option_name ${_PREFIX}USE_${option_name_base})
-    set(option_desc "CONAN")
-
-    if (NOT USE_PACKAGE_MANAGER)
-        set(sysopt "SYSTEM")
-        set(default "SYSTEM")
-        set(option_desc "SYSTEM (forced)")
-        set(system_only ON)
-    elseif (ARG_PKG_CONFIG OR ARG_FIND_PACKAGE)
-        set(sysopt "SYSTEM")
-        string(PREPEND option_desc "SYSTEM (if available), ")
-        set(default "${${_PREFIX}LIB_PREFERENCE}")
-    else ()
-        set(default "CONAN")
-    endif ()
-
-    if (ARG_BY_DEFAULT_DISABLED)
-        set(default "OFF")
-    endif()
-
-    if (NOT ARG_REQUIRED)
-        set(reqopt "OFF")
-        string(APPEND option_desc ", OFF")
-    endif ()
-
-    cmd_option(${option_name}
-            "Use ${option_name_base} library [${option_desc}]"
-            "${default}"
-            STRINGS ${sysopt} "CONAN" ${reqopt}
-            )
-    # Early bail out
-    if (${option_name} STREQUAL "OFF")
-
-        message(STATUS "${package} disabled")
-
-        set(USE_${symbol} OFF CACHE INTERNAL "" FORCE)
-
-        return()
-    endif ()
-
-    set(USE_${symbol} ON CACHE INTERNAL "" FORCE)
-
-    if (TARGET "${package}")
-        return()
-    endif ()
-
-    if (${option_name} STREQUAL "SYSTEM")
-        if (ARG_PKG_CONFIG AND PKG_CONFIG_FOUND)
-            pkg_check_modules(PKG_${package} ${ARG_PKG_CONFIG})
-
-            if (PKG_${package}_FOUND)
-                message(STATUS "Using '${package}' system library (Found by pkg_config)")
-
-                # Create the target interface library
-                add_library(${interface_name} INTERFACE IMPORTED GLOBAL)
-
-                # Retrieve the package information
-                get_package_interface(PKG_${package})
-
-                # And add it to our target
-                target_include_directories(${interface_name} INTERFACE ${INCLUDES})
-                target_link_libraries(${interface_name} INTERFACE ${LIBRARIES})
-
-                message(STATUS "Added inteface ${interface_name} ${INCLUDES} ${LIBRARIES}")
-                return()
-            endif ()
-        endif ()
-
-        if (ARG_FIND_PACKAGE OR NOT PKG_CONFIG_FOUND)
-            if (ARG_REQUIRED)
-                list(APPEND find_package_options REQUIRED)
-            endif ()
-
-            find_package(${package} ${find_package_options})
-
-            if (${package}_FOUND)
-                message(STATUS "Using '${package}' system library (Found by find_package)")
-                return()
-            endif ()
-        endif ()
-
-        if (system_only)
-            if (ARG_REQUIRED)
-                message(FATAL_ERROR "Failed to find the required system package ${package}")
-            endif ()
-
-            message(STATUS "\tFailed to find the system package ${package}")
-
-            set(USE_${symbol} OFF CACHE INTERNAL "" FORCE)
-            set(${option_name} "OFF")
-            set_property(CACHE ${option_name} PROPERTY VALUE "OFF")
-
-            return()
-        else ()
-            set(${option_name} "CONAN")
-            set_property(CACHE ${option_name} PROPERTY VALUE "CONAN")
-            message(STATUS "'${package}' not found on system, falling back to conan")
-        endif ()
-    endif ()
-
-    if (ARG_CONAN_PKG_NAME)
-        set(conan_package_name_only ${ARG_CONAN_PKG_NAME})
-    else ()
-        # We only want the name of the library
-        string(REPLACE "/" ";" pkg_name_split ${conan_package_name})
-        list(GET pkg_name_split 0 conan_package_name_only)
-    endif ()
-
-    list(APPEND CONAN_REQUIRES ${conan_package_name})
-    list(APPEND CONAN_PACKAGE_OPTIONS ${ARG_CONAN_OPTIONS})
-    list(APPEND CONAN_RESOLVE_LIST "${conan_package_name_only}|${interface_name}")
-
-    if (ARG_HAS_ONLY_DEBUG_RELEASE)
-        message(STATUS "${package} only has Debug and Release versions")
-        list(APPEND CONAN_ONLY_DEBUG_RELEASE ${package})
-    endif ()
-
-    lift_var(CONAN_REQUIRES
-            CONAN_PACKAGE_OPTIONS
-            CONAN_RESOLVE_LIST
-            CONAN_ONLY_DEBUG_RELEASE)
-
-    message(STATUS "Adding Conan dependency ${package}")
-endfunction()
-
-function(_conan_install build_type)
-    conan_cmake_configure(
-            REQUIRES ${CONAN_REQUIRES}
-            GENERATORS cmake_multi
-            BUILD_REQUIRES ${CONAN_BUILD_REQUIRES}
-            ${CONAN_CONFIG_OPTIONS}
-            IMPORTS "bin, *.dll -> ${RUNTIME_OUTPUT_DIRECTORY} @ keep_path=False"
-            IMPORTS "lib, *.dll -> ${RUNTIME_OUTPUT_DIRECTORY} @ keep_path=False"
-            IMPORTS "lib, *.dylib -> ${RUNTIME_OUTPUT_DIRECTORY} @ keep_path=False"
-            IMPORTS "lib, *.so* -> ${RUNTIME_OUTPUT_DIRECTORY} @ keep_path=False"
-            OPTIONS ${CONAN_PACKAGE_OPTIONS}
-    )
-
-    # Hash the conanfile.txt to check if it has changed, so we can skip running conan
-    set(hash_name "${build_type}_CONAN_HASH")
-    file(MD5 "${CMAKE_CURRENT_BINARY_DIR}/conanfile.txt" conanfile_hash)
-
-    if ("_${conanfile_hash}" STREQUAL "_${${hash_name}}" AND EXISTS "${CMAKE_CURRENT_BINARY_DIR}/conanbuildinfo_multi.cmake")
-        message(STATUS "Conan up to date for ${build_type}, not running Conan")
-        return()
-    endif ()
-
-    set(${hash_name} ${conanfile_hash} CACHE INTERNAL "MD5 hash of conanfile.txt for ${build_type}" FORCE)
-
-    message(STATUS "Configuring packages for ${build_type}")
-
-    conan_cmake_autodetect(settings BUILD_TYPE ${build_type})
-
-    if (build_type MATCHES "MinSizeRel|RelWithDebInfo")
-        message(STATUS "Release only libraries: ${CONAN_ONLY_DEBUG_RELEASE}")
-
-        foreach (package ${CONAN_ONLY_DEBUG_RELEASE})
-            list(APPEND settings "${package}:build_type=Release")
-        endforeach ()
-    endif ()
-
-    message(STATUS
-            "Executing Conan: \
-        REQUIRES ${CONAN_REQUIRES}
-        GENERATORS cmake_multi
-        BUILD_REQUIRES ${CONAN_BUILD_REQUIRES}
-        ${CONAN_CONFIG_OPTIONS}
-        OPTIONS ${CONAN_PACKAGE_OPTIONS}
-        SETTINGS ${settings}
-    ")
-
-    conan_cmake_install(PATH_OR_REFERENCE .
-            BUILD missing
-            SETTINGS ${settings}
-            )
-endfunction()
-
-macro(resolve_conan_dependencies)
-    if (USE_PACKAGE_MANAGER)
-        if (MSVC OR XCODE AND NOT DEFINED CMAKE_BUILD_TYPE)
-            foreach (TYPE ${CMAKE_CONFIGURATION_TYPES})
-                _conan_install(${TYPE})
-            endforeach ()
-        else ()
-            _conan_install(${CMAKE_BUILD_TYPE})
-        endif ()
-
-        list(REMOVE_DUPLICATES CONAN_REQUIRES)
-
-        set(CONAN_CMAKE_SILENT_OUTPUT TRUE)
-        include(${CMAKE_CURRENT_BINARY_DIR}/conanbuildinfo_multi.cmake)
-        conan_define_targets()
-
-        foreach (package_raw ${CONAN_RESOLVE_LIST})
-            string(REPLACE "|" ";" package_list ${package_raw})
-            list(GET package_list 0 package)
-            list(GET package_list 1 target_name)
-
-            # Alias the CONAN_PKG target to the propper target name
-            set(_curr_conan_target "CONAN_PKG::${package}")
-            if (TARGET ${_curr_conan_target})
-                set_target_properties(${_curr_conan_target} PROPERTIES IMPORTED_GLOBAL TRUE)
-                add_library(${target_name} ALIAS ${_curr_conan_target})
-            else ()
-                message(WARNING "Conan target ${_curr_conan_target} not found")
-            endif ()
-        endforeach ()
-
-    endif (USE_PACKAGE_MANAGER)
-
-    file(GLOB dependency_helpers "${CMAKE_SOURCE_DIR}/cmake/dependencies-helpers/*.cmake")
-    foreach (f ${dependency_helpers})
-        include(${f})
-    endforeach ()
-endmacro()
-
-# Helper to retrieve the settings returned from pkg_check_modules()
-macro(get_package_interface package)
-    set(INCLUDES ${${package}_INCLUDE_DIRS})
-
-    set(LINKDIRS ${${package}_LIBDIR})
-
-    # We resolve the full path of each library to ensure the
-    # correct one is referenced while linking
-    foreach (lib ${${package}_LIBRARIES})
-        find_library(LIB_${lib} ${lib} HINTS ${LINKDIRS})
-        list(APPEND LIBRARIES ${LIB_${lib}})
-    endforeach ()
-endmacro()

external/imgui/CMakeLists.txt
@@ -17,5 +17,5 @@ set(IMGUI_SOURCES
 
 add_library(IMGUI STATIC ${IMGUI_SOURCES})
 
-target_link_libraries(IMGUI ${TESTS_PROJECT_NAME} PUBLIC SDL::SDL)
+target_link_libraries(IMGUI ${TESTS_PROJECT_NAME} PUBLIC SDL2::SDL2)
 target_include_directories(IMGUI PUBLIC . backends)

commit 84e439d51f732a75c8a629194c0f3a6460e4c853

hotfix: missing NOISE_IN_SUBDIR conditional define
Compiling fails if NOISE_IN_SUBDIR include-route is not defined.
Restored conditional-include-code and fixed regression detected onwards
from commit: d1e96913.

src/engine/map/TerrainGenerator.cxx
@@ -8,9 +8,11 @@
 #include <PointFunctions.hxx>
 
 #include "json.hxx"
-
+#ifdef NOISE_IN_SUBDIR
 #include <noise/noise.h>
-
+#else
+#include <noise.h>
+#endif
 #include <random>
 
 using json = nlohmann::json;